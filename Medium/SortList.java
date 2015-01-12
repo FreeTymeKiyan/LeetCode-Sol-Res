95f7ca6a22717e1128d79c95de4f089c681fb0cc
Kiyan <freetymesunkiyan@gmail.com>
2015-01-12 11:49:24 -0500

    Updated sort list

--- Medium/SortList.java
+++ Medium/SortList.java
@@ -10,7 +10,7 @@ class SortList {
     }
 
     /**
-     * 
+     * Get list length and do merge sort
      */
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) return head;
@@ -20,12 +20,17 @@ class SortList {
             tail = tail.next;
             len++;
         }
-        head = mergeSort(null, head, len);
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        dummy.next = head;
+        head = mergeSort(dummy, head, len);
         return head;
     }
     
     /**
-     * 
+     * Cut into two halves
+     * Sort left half first, move to right half's pre head and sort right
+     * Merge two halves
+     * Insert node in latter part if its smaller than current node
      */
     public ListNode mergeSort(ListNode preHead, ListNode head, int len) {
         if (head == null || len <= 1) return head;
@@ -38,17 +43,17 @@ class SortList {
         for (int i = 0; i < left - 1; i++) pMid = pMid.next;
         mergeSort(pMid, pMid.next, right);
         // merge
-        ListNode p1 = head;
-        ListNode p2 = pMid.next;
         ListNode pre1 = preHead;
+        ListNode p1 = head;
         ListNode pre2 = pMid;
-        if (p1.val > p2.val) head = p2;
+        ListNode p2 = pMid.next;
+        if (p1.val > p2.val) head = p2; // switch head
         while (left > 0 && right > 0) {
             // merge second half to first half
             if (p1.val > p2.val) {
                 pre2.next = p2.next; // insert p2 before p1
                 p2.next = p1;
-                if (pre1 != null) pre1.next = p2;
+                pre1.next = p2;
                 // set to next
                 pre1 = p2;
                 p2 = pre2.next;
