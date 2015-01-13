11bdd083f35ae2690f5801b7b47640a64b3416a5
Kiyan <freetymesunkiyan@gmail.com>
2015-01-12 23:37:57 -0500

    Updated LRUCache

--- Hard/LRUCache.java
+++ Hard/LRUCache.java
@@ -1,45 +1,54 @@
 /**
  * Design and implement a data structure for Least Recently Used (LRU) cache.
  * It should support the following operations: get and set.
- * 
- * get(key) - Get the value (will always be positive) of the key if the key 
+ *
+ * get(key) - Get the value (will always be positive) of the key if the key
  * exists in the cache, otherwise return -1.
  * (get means use, so we need to update)
- * 
+ *
  * set(key, value) - Set or insert the value if the key is not already present.
- * When the cache reached its capacity, it should invalidate the least recently 
+ * When the cache reached its capacity, it should invalidate the least recently
  * used item before inserting a new item.
- * 
+ *
  * Tags: Data Structure
  */
 class LRUCache {
 
+    /**
+     * Doubly linked list node
+     */
     static class Node{
-        Node next; // double linked node 
+        Node next;
         Node prev;
         int key;
         int val;
-        Node(){}
-        Node(int key, int val){this.key = key; this.val = val;}
 
-        private void delete(){
-        prev.next = next;
-        if(next != null)
-            next.prev = prev;
+        Node (int key, int val) {
+            this.key = key;
+            this.val = val;s
         }
 
-        private void addAfter(Node preNode){
+        /**
+         * Delete current node
+         */
+        private void delete() {
+            prev.next = next;
+            if (next != null) next.prev = prev;
+        }
+
+        /**
+         * Add current node to preNode's next
+         */
+        private void addAfter(Node preNode) {
             next = preNode.next;
-            if(next != null)
-                next.prev = this;
+            if (next != null) next.prev = this;
             preNode.next = this;
             prev = preNode;
         }
     }
-    
-    Node head = new Node();
-    Node tail = head;
 
+    Node dummy = new Node();
+    Node tail = dummy;
     Map<Integer, Node> cache = new HashMap<Integer, Node>();
     int capacity;
 
@@ -48,62 +57,69 @@ class LRUCache {
     }
 
     public int get(int key) {
+        if (!map.containsKey(key)) return -1;
         Node n = cache.get(key);
-        if(n == null)
-            return -1;
         moveToTail(n);
         return n.val;
     }
 
+    /**
+     * Check key in map or not
+     * If in map, get node, update value, and move to tail
+     * If not, create node, put in cache, and add to tail
+     * If capacity exceeds, remove head from map and delete
+     */
     public void set(int key, int value) {
-        Node n = cache.get(key);
-        if(n != null){
+        if (map.containsKey(key)) {
+            Node n = cache.get(key);
             n.val = value;
             moveToTail(n);
             return;
         }
-
-        n = new Node(key, value); // add new node
+        Node n = new Node(key, value); // add new node
         cache.put(key, n);
         addToTail(n); // update usage
 
-        if(cache.size() > capacity){
-            n = head.next;
+        if (cache.size() > capacity){
+            n = dummy.next;
             cache.remove(n.key);
-            n.delete(); // delete head
+            n.delete(); // delete dummy
         }
     }
 
     private void moveToTail(Node n){
-        if(n == tail)
-            return;
-        n.delete();
-        addToTail(n);
+        if (n == tail) return;
+        n.delete(); // unlink node with other nodes first
+        addToTail(n); // add it to tail
     }
 
+    /**
+     * Add node after tail and update tail to that node
+     */
     private void addToTail(Node n){
         n.addAfter(tail);
-        tail = n;       
+        tail = n;
     }
-    
-    
-    // TLE, use two lists and sync manually
+
+    /**
+     * TLE, use two lists and sync manually
+     */
     class LRUCacheOwn {
-    
+
         List<Integer> keys = null;
         List<Integer> values = null;
         int capacity = -1;
-    
+
         public LRUCache(int capacity) {
             this.capacity = capacity;
             this.keys = new ArrayList<Integer>(capacity);
             this.values = new ArrayList<Integer>(capacity);
         }
-    
+
         public int get(int key) {
             int index = keys.indexOf(key);
             if (index == -1) {
-                return -1;    
+                return -1;
             }
             keys.remove(index);
             keys.add(key);
@@ -111,7 +127,7 @@ class LRUCache {
             values.add(v);
             return v;
         }
-    
+
         public void set(int key, int value) {
             int index = keys.indexOf(key);
             if (index != -1) {
