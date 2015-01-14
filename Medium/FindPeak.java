3014cb2bde67b5cfeed6d1eca1bc3e5500cc719b
Kiyan <freetymesunkiyan@gmail.com>
2015-01-13 20:53:11 -0500

    Updated find peak

--- Medium/FindPeak.java
+++ Medium/FindPeak.java
@@ -18,29 +18,26 @@ class FindPeak {
     
     public static void main(String[] args) {
         int[] num = {1, 2, 1, 3, 1, 4, 1};
-        System.out.println(findPeakElement(num));
+        System.out.println(new FindPeak().findPeakElement(num));
     }
     
     /**
-     * 
+     * Binary search for a peak. Other peaks can be ignored. 
      */
-    public static int findPeakElement(int[] num) {
+    public int findPeakElement(int[] num) {
         if (num == null || num.length == 0) return 0;
         int n = num.length;
         if (n <= 1) return 0;
         // handle the first and last element in num[]
         if (num[0] > num[1]) return 0;
         if (num[n - 1] > num[n - 2]) return n - 1;
+        
         int left = 1, right = n - 2;
         while (left < right) {
-            int mid = (left + right) >> 1;
-            if (num[mid] > num[mid - 1] && num[mid] > num[mid + 1]) {
-                return mid;
-            } else if (num[mid] > num[mid + 1]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+            int mid = (right - left) / 2 + left;
+            if (num[mid] > num[mid - 1] && num[mid] > num[mid + 1]) return mid;
+            else if (num[mid] > num[mid + 1]) right = mid - 1;
+            else left = mid + 1;
         }
         return left;
     }
