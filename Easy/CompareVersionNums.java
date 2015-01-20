2505e5d5bf5ae8da7f1d52f2909718fe5bd3131d
Kiyan <freetymesunkiyan@gmail.com>
2015-01-19 19:49:47 -0600

    Updated compare version nums

--- Easy/CompareVersionNums.java
+++ Easy/CompareVersionNums.java
@@ -27,10 +27,10 @@ class CompareVersionNums {
      * Compare each level and compare the rest
      * Note the input can be complex than the example, more dots, more zeros
      */
-    public static int compareVersion(String version1, String version2) {
-        if (version1 == null && version2 == null) return 0;
+    int compareVersion(String version1, String version2) {
+        if (version1 == null && version2 == null) return 0; // same
         if (version1 == null || version2 == null) return version1 == null ? -1 : 1;
-        String[] list1 = version1.split("\\.");
+        String[] list1 = version1.split("\\."); // back slash
         String[] list2 = version2.split("\\.");
         int i = 0;
         while (i < list1.length || i < list2.length) {
@@ -38,7 +38,7 @@ class CompareVersionNums {
             int b = i < list2.length ? Integer.valueOf(list2[i]) : 0;
             if (a < b) return -1;
             else if (a > b) return 1;
-            i++;
+            i++; // update i
         }
         return 0;
     }
