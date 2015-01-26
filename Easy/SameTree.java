f2eb4e713cb9ae40ffc04ae28a58455244b8e20e
Kiyan <freetymesunkiyan@gmail.com>
2015-01-26 11:41:17 -0500

    Updated Same Tree

--- Easy/SameTree.java
+++ Easy/SameTree.java
@@ -19,7 +19,7 @@ class SameTree {
      */
     public static boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null || q == null) return p == q; // if one of them is null, it will return false. both null, true.
-        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right); // equal val, equal subtrees
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right); // equal val, equal subtrees
     }
     
     public class TreeNode {
