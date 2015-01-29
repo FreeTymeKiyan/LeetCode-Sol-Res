b7ac174ca177e37d043c1d45178faea6ecb496de
Kiyan <freetymesunkiyan@gmail.com>
2015-01-29 16:57:58 -0500

    Updated 3 sum

--- Medium/ThreeSum.java
+++ Medium/ThreeSum.java
@@ -23,40 +23,37 @@ import java.util.Arrays;
  */
 class ThreeSum {
     public static void main(String[] args) {
-        int[] s = { -1, 0, 1, 2, -1, -4};
-        printResult(threeSum(s));
+        ThreeSum t = new ThreeSum();
+        int[] s = { -1, 0, 1, 2, -1, -4 };
+        t.printResult(t.threeSum(s));
     }
 
     /**
-     * Sort the given array. 
+     * Two Pointers.
+     * Sort given array first.
+     * Traverse the array with 1 pointer
+     * Use 2 more pointers from both start(i + 1) and end to find target
      */
     public static List<List<Integer>> threeSum(int[] num) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         Arrays.sort(num);
 
         for (int i = 0; i < num.length - 2; i++) {
-            if (i > 0 && num[i] == num[i - 1]) {
-                continue; // for duplicate situation
-            }
-            if (num[i] > 0) {
-                break; // can stop at positive integers
-            }
+            if (i > 0 && num[i] == num[i - 1]) continue; // skip duplicate 
+            if (num[i] > 0) break; // stop at positive integers
             
             int j = i + 1;
             int k = num.length - 1;
             while (j < k) {
-                if (j > i + 1 && num[j] == num[j - 1]) {
+                if (j > i + 1 && num[j] == num[j - 1]) { // skip duplicate 
                     j++;
                     continue;
                 }
-                if (num[i] + num[j] > 0) { // already bigger than 0
-                    break;
-                }
-                if (num[i] + num[j] + num[k] < 0) {
-                    j++;
-                } else if (num[i] + num[j] + num[k] > 0) {
-                    k--;
-                } else {
+                if (num[i] + num[j] > 0) break;// already bigger than 0
+                
+                if (num[i] + num[j] + num[k] < 0) j++;
+                else if (num[i] + num[j] + num[k] > 0) k--;
+                else { // num[i] + num[j] + num[k] == 0
                     List<Integer> triplets = new ArrayList<Integer>();
                     triplets.add(num[i]);
                     triplets.add(num[j]);
