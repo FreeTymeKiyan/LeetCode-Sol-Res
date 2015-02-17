312b032dbf50e3865d37d9381b383ad8077dece0
Kiyan <freetymesunkiyan@gmail.com>
2015-02-16 20:58:05 -0500

    Updated wildcard matching

--- Hard/WildcardMatching.java
+++ Hard/WildcardMatching.java
@@ -25,99 +25,64 @@
  */
 class WildcardMatching {
     public static void main(String[] args) {
+        WildcardMatching w = new WildcardMatching();
         /*input validation*/
-        // System.out.println(isMatch("", "a")); // false
-        // System.out.println(isMatch("a", "")); // false
-        // System.out.println(isMatch("", "")); // true
-        // System.out.println(isMatch(null, null)); // true
-        // System.out.println(isMatch("a", null)); // false
-        // System.out.println(isMatch(null, "null")); // false
-        /*letters*/
-        // System.out.println(isMatch("a", "aa")); // false
-        // System.out.println(isMatch("aa", "a")); // false
-        // System.out.println(isMatch("aa", "aa")); // true
-        // System.out.println(isMatch("aa", "ab")); // false
-        /**s*/
-        // System.out.println(isMatch("aa", "*")); // true
-        /*?s*/
-        // System.out.println(isMatch("aa", "?")); // false
-        // System.out.println(isMatch("a", "?")); // true
-        /*letters and *s*/
-        // System.out.println(isMatch("abc", "a*")); // true
-        // System.out.println(isMatch("ab", "a*")); // true
-        // System.out.println(isMatch("ab", "*a")); // false
-        // System.out.println(isMatch("a", "a*")); // true
-        // System.out.println(isMatch("bcsa", "*a")); // true
-        // System.out.println(isMatch("bcs", "*a")); // false
-        // System.out.println(isMatch("bbbbbbbbbb", "*bbbbb")); // true
+        // System.out.println(w.isMatch("", "a")); // false
+        // System.out.println(w.isMatch("a", "")); // false
+        // System.out.println(w.isMatch("", "")); // true
+        // System.out.println(w.isMatch(null, null)); // true
+        // System.out.println(w.isMatch("a", null)); // false
+        // System.out.println(w.isMatch(null, "null")); // false
+        /*letters*/           
+        // System.out.println(w.isMatch("a", "aa")); // false
+        // System.out.println(w.isMatch("aa", "a")); // false
+        // System.out.println(w.isMatch("aa", "aa")); // true
+        // System.out.println(w.isMatch("aa", "ab")); // false
+        /**s*/                
+        // System.out.println(w.isMatch("aa", "*")); // true
+        /*?s*/                
+        // System.out.println(w.isMatch("aa", "?")); // false
+        // System.out.println(w.isMatch("a", "?")); // true
+        /*letters and *s*/    
+        // System.out.println(w.isMatch("abc", "a*")); // true
+        // System.out.println(w.isMatch("ab", "a*")); // true
+        // System.out.println(w.isMatch("ab", "*a")); // false
+        // System.out.println(w.isMatch("a", "a*")); // true
+        // System.out.println(w.isMatch("bcsa", "*a")); // true
+        // System.out.println(w.isMatch("bcs", "*a")); // false
+        // System.out.println(w.isMatch("bbbbbbbbbb", "*bbbbb")); // true
 
         /* * and ? */
         // System.out.println(isMatch("b", "*?*?")); // false
     }
 
     /**
-     * Two pointers
+     * DP, two pointers
      * remember the index of * and matched sequence
      * advance only pattern pointer when * is found
      * match the sequence after * in pattern with the rest of the string
      */
-    public static boolean isMatch(String str, String pattern) {
+    public boolean isMatch(String str, String pattern) {
         if (str == null && pattern == null) return true;
         if (str == null || pattern == null) return false;
+        
         int s = 0, p = 0, match = 0, astroIdx = -1;
-        while (s < str.length()){
-            // move both pointers
-            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
-                s++;
+        while (s < str.length()) {
+            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){ // found ? or same chars
+                s++; // move both pointers
                 p++;
-            }
-            // * found, only move pattern pointer
-            else if (p < pattern.length() && pattern.charAt(p) == '*'){
-                astroIdx = p;
-                match = s;
-                p++;
-            }
-            // last pattern pointer was *, move string pointer
-            else if (astroIdx != -1){
-                p = astroIdx + 1; // move to next of *
-                match++;
+            } else if (p < pattern.length() && pattern.charAt(p) == '*') { // found *
+                astroIdx = p; // save astroid index in pattern
+                match = s; // save current index of string
+                p++; // only move pattern pointer forward
+            } else if (astroIdx != -1){ // try to find last astroid
+                p = astroIdx + 1; // move to * one char behind astroid
+                match++; // move current index of string
                 s = match;
-            }
-            //current pattern pointer is not star, last patter pointer was not *
-            //characters do not match
-            else return false;
+            } else return false; // not ?, not same char, not *, don't match
         }
-
-        //check for remaining characters in pattern
+        // check remaining characters in pattern, can only be astroid
         while (p < pattern.length() && pattern.charAt(p) == '*') p++;
-        return p == pattern.length();
+        return p == pattern.length(); // no remaining
     }
-
-    /**
-     * validate input
-     * seperate letter, * and ?
-     * how can be a match? letters only, * only, ? only
-     * letters and *, letters and ?, * and ?
-     * letters, * and ?
-     * check current char, return the result of current and latter ones
-     */
-    public static boolean isMatch(String s, String p) {
-        // System.out.println(s + " | " + p);
-        if (s == null && p == null) return true;
-        if (s == null || p == null) return false;
-        if (s.equals("") || p.equals("")) return hasAstro || p.equals("*") || p.equals(s);
-        for (int i = 0; i < p.length(); i++) {
-            char fromP = p.charAt(i);
-            char fromS = s.charAt(i);
-            if (fromP == '*') { // problem here with astroid
-                return isMatch(s, p.substring(i + 1));
-            } else if (fromP == '?'|| fromP == fromS) {
-                return isMatch(s.substring(i + 1), p.substring(i + 1));
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-    
 }
