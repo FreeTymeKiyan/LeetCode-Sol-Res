776043277c8dc06145739fdca1f308ee0b0a3740
Kiyan <freetymesunkiyan@gmail.com>
2015-02-21 17:07:10 -0500

    Updated pow

--- Medium/Pow.java
+++ Medium/Pow.java
@@ -10,14 +10,19 @@ class Pow {
     }
     
     /**
-     * Divide n by 2
-     * 
+     * Binary Search, divide n by 2
      * Questions: 
      * 1. can x be zero?
      * 2. can n be negative?
+     *
      * When n is odd, multiply result by f
      * f multiply by itself each time
-     * Repeat until n <= 0
+     * Repeat until n == 0
+     * x^10 = x^2 * x^8
+     * x^9 = x * x^8
+     * x^8 = x^8
+     * x^7 = x * x^2 * x^4
+     * x^6 = x^2 * x^4
      */
     public double pow(double x, int n) {
         if (n == 0) return 1;
@@ -26,9 +31,9 @@ class Pow {
             x = 1 / x; // x can be zero?
         }
         double res = 1; // mind overflow
-        for (double f = x; n > 0; n = n >> 1) { // n/=2
+        for (double f = x; n > 0; n = n >> 1) { // n >>= 1, n/=2
             if (n % 2 == 1) res *= f;
-            f = f * f;
+            f = f * f; // f *= f
         }
         return res;
     }
