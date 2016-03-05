e13bb01ebaa630969b18fdb40b7e780e586bd7f4
Linsen Wu <forest5013@gmail.com>
2016-03-05 11:28:06 -0600

    change all tabs to 4 spaces

--- C++/208_Implement_Trie_(Prefix_Tree).cpp
+++ C++/208_Implement_Trie_(Prefix_Tree).cpp
@@ -18,12 +18,12 @@ using namespace std;
 
 class TrieNode {
 public:
-	TrieNode *child[26];
-	bool isWord;
+    TrieNode *child[26];
+    bool isWord;
     // Initialize your data structure here.
     TrieNode() {
-        for (auto &a : child) a = NULL;		//initialize the child point to be NULL
-		isWord =  false;					//initialize the whether is an existing word to be false
+        for (auto &a : child) a = NULL;        //initialize the child point to be NULL
+        isWord =  false;                    //initialize the whether is an existing word to be false
     }
 };
 
@@ -35,68 +35,68 @@ public:
 
     // Inserts a word into the trie.
     void insert(string word) {
-		if (word.size() == 0)
-		{
-			return;
-		}
+        if (word.size() == 0)
+        {
+            return;
+        }
 
-		TrieNode *next = root;
-		for (int i = 0; i < word.size(); i++)
-		{
-			if (next->child[word[i]-'a'] == NULL)		//If the char is not in the tree, add it to the tree. 
-			{
-				TrieNode* node = new TrieNode();
-				next->child[word[i]-'a'] = node;
-			}
-			next = next->child[word[i]-'a'];			//Otherwise go to next node directly.
-		}
-		next->isWord = true;
+        TrieNode *next = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (next->child[word[i]-'a'] == NULL)        //If the char is not in the tree, add it to the tree. 
+            {
+                TrieNode* node = new TrieNode();
+                next->child[word[i]-'a'] = node;
+            }
+            next = next->child[word[i]-'a'];            //Otherwise go to next node directly.
+        }
+        next->isWord = true;
     }
 
     // Returns if the word is in the trie.
     bool search(string word) {
-		if (word.size() == 0)
-		{
-			return true;
-		}
+        if (word.size() == 0)
+        {
+            return true;
+        }
 
-		TrieNode *next = root;
-		for (int i = 0; i < word.size(); i++)
-		{
-			if (next->child[word[i]-'a'] == NULL)		//return false if the char is not in the tree
-			{
-				return false;
-			}
-			next = next->child[word[i]-'a'];
-		}
+        TrieNode *next = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (next->child[word[i]-'a'] == NULL)        //return false if the char is not in the tree
+            {
+                return false;
+            }
+            next = next->child[word[i]-'a'];
+        }
 
-		if (next->isWord == true)						//If the last char is then end of a word, return true.
-		{
-			return true;
-		}
-		
-		return false;
+        if (next->isWord == true)                        //If the last char is then end of a word, return true.
+        {
+            return true;
+        }
+        
+        return false;
     }
 
     // Returns if there is any word in the trie
     // that starts with the given prefix.
-    bool startsWith(string prefix) {					//Similar to search but do not need to judge whether it's a word or not.
-		if (prefix.size() == 0)
-		{
-			return true;
-		}
+    bool startsWith(string prefix) {                    //Similar to search but do not need to judge whether it's a word or not.
+        if (prefix.size() == 0)
+        {
+            return true;
+        }
 
-		TrieNode *next = root;
-		for (int i = 0; i < prefix.size(); i++)
-		{
-			if (next->child[prefix[i]-'a'] == NULL)
-			{
-				return false;
-			}
-			next = next->child[prefix[i]-'a'];
-		}
-		
-		return true;        
+        TrieNode *next = root;
+        for (int i = 0; i < prefix.size(); i++)
+        {
+            if (next->child[prefix[i]-'a'] == NULL)
+            {
+                return false;
+            }
+            next = next->child[prefix[i]-'a'];
+        }
+        
+        return true;        
     }
 
 private:
@@ -110,6 +110,6 @@ private:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+    return 0;
 }
 
--- C++/211_Add_and_Search_Word _-_Data_structure_design.cpp
+++ C++/211_Add_and_Search_Word _-_Data_structure_design.cpp
@@ -35,12 +35,12 @@ using namespace std;
 
 class TrieNode {
 public:
-	TrieNode *child[26];
-	bool isWord;
+    TrieNode *child[26];
+    bool isWord;
     // Initialize your data structure here.
     TrieNode() {
         for (auto &a : child) a = NULL;
-		isWord =  false;
+        isWord =  false;
     }
 };
 
@@ -51,73 +51,73 @@ public:
     }
     // Adds a word into the data structure.
     void addWord(string word) {
-		if (word.size() == 0)
-		{
-			return;
-		}
+        if (word.size() == 0)
+        {
+            return;
+        }
 
-		TrieNode *next = root;
-		for (int i = 0; i < word.size(); i++)
-		{
-			if (next->child[word[i]-'a'] == NULL)
-			{
-				TrieNode* node = new TrieNode();
-				next->child[word[i]-'a'] = node;
-			}
-			next = next->child[word[i]-'a'];
-		}
-		next->isWord = true;        
+        TrieNode *next = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (next->child[word[i]-'a'] == NULL)
+            {
+                TrieNode* node = new TrieNode();
+                next->child[word[i]-'a'] = node;
+            }
+            next = next->child[word[i]-'a'];
+        }
+        next->isWord = true;        
     }
 
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
     bool search(string word) {
-		if (word.size() == 0)
-		{
-			return true;
-		}
+        if (word.size() == 0)
+        {
+            return true;
+        }
 
-		vector<TrieNode *> pre; //for all current possible nodes
-		vector<TrieNode *> now; //for all next round possible nodes
-		pre.push_back(root);
-		
-		for (int i = 0; i < word.size(); i++)
-		{
-			while (!pre.empty())
-			{
-				TrieNode *next = pre.back();
-				if (word[i] == '.'){
-					for (int j = 0; j < 26; j++)
-					{
-						if (next->child[j] != NULL)
-						{
-							now.push_back(next->child[j]); //push possible nodes to the next round vector
-							if (i == word.size()-1 && next->child[j]->isWord == true){ //if the search string ends with '.', there must be at least one isWord true to return true.
-								return true;
-							}
-						}
-					}
-					
-				}
-				else if(next->child[word[i]-'a'] != NULL){
-					next = next->child[word[i]-'a'];
-					now.push_back(next); //push possible nodes to the next round vector
-					if (i == word.size()-1 && next->isWord == true){
-						return true;
-					}
-				}
-				pre.pop_back(); //pop out current node once finished searching
-			}
+        vector<TrieNode *> pre; //for all current possible nodes
+        vector<TrieNode *> now; //for all next round possible nodes
+        pre.push_back(root);
+        
+        for (int i = 0; i < word.size(); i++)
+        {
+            while (!pre.empty())
+            {
+                TrieNode *next = pre.back();
+                if (word[i] == '.'){
+                    for (int j = 0; j < 26; j++)
+                    {
+                        if (next->child[j] != NULL)
+                        {
+                            now.push_back(next->child[j]); //push possible nodes to the next round vector
+                            if (i == word.size()-1 && next->child[j]->isWord == true){ //if the search string ends with '.', there must be at least one isWord true to return true.
+                                return true;
+                            }
+                        }
+                    }
+                    
+                }
+                else if(next->child[word[i]-'a'] != NULL){
+                    next = next->child[word[i]-'a'];
+                    now.push_back(next); //push possible nodes to the next round vector
+                    if (i == word.size()-1 && next->isWord == true){
+                        return true;
+                    }
+                }
+                pre.pop_back(); //pop out current node once finished searching
+            }
 
-			pre = now;
-			now.clear();
-		}
-	
-		return false;        
+            pre = now;
+            now.clear();
+        }
+    
+        return false;        
     }
 
-	private:
-		TrieNode* root;
+    private:
+        TrieNode* root;
 };
 
 // Your WordDictionary object will be instantiated and called as such:
@@ -127,13 +127,13 @@ public:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	WordDictionary _inst;
-	_inst.addWord("a");
-	_inst.addWord("a");
-	_inst.search("aa");
-	_inst.search("a");
-	_inst.search(".a");
-	_inst.search("a.");
-	return 0;
+    WordDictionary _inst;
+    _inst.addWord("a");
+    _inst.addWord("a");
+    _inst.search("aa");
+    _inst.search("a");
+    _inst.search(".a");
+    _inst.search("a.");
+    return 0;
 }
 
