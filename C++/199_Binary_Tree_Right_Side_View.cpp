d57c52208d8110cabd8604b4b984df7291628a55
Yanbin Lu <lvyb20829@gmail.com>
2016-03-05 22:58:27 -0600

    Format adjustment

--- C++/199_Binary_Tree_Right_Side_View.cpp
+++ C++/199_Binary_Tree_Right_Side_View.cpp
@@ -1,4 +1,3 @@
-#include <stddef.h>
 // 199 Binary Tree Right Side View
 /**
 * Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
@@ -16,7 +15,7 @@
 *
 * Author:  Yanbin Lu
 */
-
+#include <stddef.h>
 #include <vector>
 #include <string.h>
 #include <stdio.h>
