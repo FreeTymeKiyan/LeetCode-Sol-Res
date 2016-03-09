e3c0cfa7fd6a4335aa67d5be151a95125e9d36d7
JoannaCode <chenshuna2012@gmail.com>
2016-03-08 17:06:39 -0700

    Update SuperUglyNumber.java

--- Medium/SuperUglyNumber.java
+++ Medium/SuperUglyNumber.java
@@ -24,7 +24,7 @@ public class Solution {
         d[0] = 1;
         int[] p = new int[primes.length];
         Arrays.fill(p,0);
-        for(int i = 1;i < n;i++){
+        for(int i = 1; i < n; i++){
             int min = Integer.MAX_VALUE;
             for(int j = 0; j < primes.length; j++){
                 min = Math.min(min,primes[j] * d[p[j]]) ;
