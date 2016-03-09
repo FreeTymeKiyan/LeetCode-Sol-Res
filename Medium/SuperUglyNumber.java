627eaf389bb2c437c04a6af54e33714a51f3ad95
JoannaCode <chenshuna2012@gmail.com>
2016-03-08 17:06:14 -0700

    Update SuperUglyNumber.java

--- Medium/SuperUglyNumber.java
+++ Medium/SuperUglyNumber.java
@@ -20,18 +20,18 @@
 
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        int[] d =new int[n];
-        d[0]=1;
+        int[] d = new int[n];
+        d[0] = 1;
         int[] p = new int[primes.length];
         Arrays.fill(p,0);
-        for(int i=1;i<n;i++){
+        for(int i = 1;i < n;i++){
             int min = Integer.MAX_VALUE;
-            for(int j=0;j<primes.length;j++){
-                min = Math.min(min,primes[j]*d[p[j]]) ;
+            for(int j = 0; j < primes.length; j++){
+                min = Math.min(min,primes[j] * d[p[j]]) ;
             }
-            d[i]=min;
-            for(int k =0;k<primes.length;k++){
-                if(min == primes[k]*d[p[k]]){
+            d[i] = min;
+            for(int k = 0; k < primes.length; k++){
+                if(min == primes[k] * d[p[k]]){
                     p[k]++;
                 }
             }
