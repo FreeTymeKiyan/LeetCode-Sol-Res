e6eb1c0057ed74d9d62eccf854cb022dd3c6ecfd
JoannaCode <chenshuna2012@gmail.com>
2016-03-21 20:31:02 -0700

    finished 115

--- Hard/DistinctSubsequences_shuna.java
+++ Hard/DistinctSubsequences_shuna.java
@@ -4,6 +4,7 @@
 * A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 * Here is an example:
 * S = "rabbbit", T = "rabbit" Return 3.
+* @author chenshuna
 * Note : DP
 */
 
