85962f8f9c17722de44c19e54e1a3bbf80140eec
JoannaCode <chenshuna2012@gmail.com>
2016-03-21 20:48:44 -0700

    add tag and similar

--- Hard/reverseKGroup.java
+++ Hard/reverseKGroup.java
@@ -8,6 +8,8 @@
  * Given this linked list: 1->2->3->4->5
  * For k = 2, you should return: 2->1->4->3->5
  * For k = 3, you should return: 3->2->1->4->5
+ * Tags: Linked list
+ * Similar Problems: Swap Nodes in Pairs
  * @author chenshuna
  */
 public class reverseKGroup {
