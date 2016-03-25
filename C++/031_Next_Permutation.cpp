c89cb1efacb26997ecdfd2f42f4fe9c273b529dd
kuangami <david.qinkuang@gmail.com>
2016-03-25 15:47:52 -0500

    Update 031_Next_Permutation.cpp
    Change unknown characters

--- C++/031_Next_Permutation.cpp
+++ C++/031_Next_Permutation.cpp
@@ -7,9 +7,9 @@
  * The replacement must be in-place, do not allocate extra memory.
  *
  * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
- * 1,2,3 ¡ú 1,3,2
- * 3,2,1 ¡ú 1,2,3
- * 1,1,5 ¡ú 1,5,1
+ * 1,2,3 -> 1,3,2
+ * 3,2,1 -> 1,2,3
+ * 1,1,5 -> 1,5,1
  *
  * Tags: Array
  *
