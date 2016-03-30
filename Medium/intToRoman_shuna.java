a4f0222639ab7bb9ae3bbf3d7c5985b69d27ab42
JoannaCode <chenshuna2012@gmail.com>
2016-03-29 17:23:20 -0700

    add 12

--- Medium/intToRoman_shuna.java
+++ Medium/intToRoman_shuna.java
@@ -12,7 +12,7 @@ public class IntegertoRoman {
         String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return M[num / 1000] + C[(num % 1000) / 100]+ X[(num % 100) / 10] + I[num % 10];
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
     }
 
     public static void main(String[] args) {
