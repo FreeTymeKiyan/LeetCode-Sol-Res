d10903b37fb67e14dd4149698cd702052571b8c9
JoannaCode <chenshuna2012@gmail.com>
2016-03-29 20:09:48 -0700

    finished 164

---
+++ Hard/MaximumGap_shuna.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+/**
+ * 164. Maximum Gap
+ * Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+ * Try to solve it in linear time/space.
+ * Return 0 if the array contains less than 2 elements.
+ * You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+ * Tags: Sort
+ * 
+ * Analysis:
+ * The key point is linear time/space: Bucket Sort
+ * @author chenshuna
+ */
+
+public class MaximumGap {
+    public static int maximumGap(int[] nums) {
+        if(nums == null || nums.length < 2){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int length = nums.length;
+        int maxGap = 0;
+        for(int i : nums){
+           max = Math.max(max, i);
+           min = Math.min(min, i);
+        }
+        if(min == max){
+            return 0;
+        }
+        int bicketSize = (max - min) / (length - 1);
+        if(bicketSize == 0){
+            bicketSize = 1;
+        }
+        int bicketCount = (max - min) / bicketSize + 1;
+        int[] left = new int[bicketCount];
+        int[] right = new int[bicketCount];
+        Arrays.fill(left, -1);
+        Arrays.fill(right, -1);
+        for(int i = 0; i < length ; i ++){
+            int loc = (nums[i] - min) / bicketSize;
+            if(left[loc] == -1){
+               left[loc] = nums[i];
+               right[loc] = nums[i];
+            }
+            else{
+                left[loc] = Math.min(left[loc], nums[i]);
+                right[loc] = Math.max(right[loc], nums[i]);
+            }
+        }
+        int locLeft;
+        int locRight = -1;
+        for(int i = 0; i < bicketCount; i++){
+           if(left[i] != -1 && locRight == -1){
+               locRight = right[i];
+           }
+           else if(left[i] != -1 && locRight != -1){
+               locLeft = left[i];
+               maxGap= Math.max(maxGap, locLeft - locRight );
+               locRight = right[i];
+           }
+        }
+        return maxGap;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(maximumGap(new int[]{1,6,7,3,4,19}));
+    }
+}
