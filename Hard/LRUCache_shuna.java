9aeabb56fa33a118335fee0976861d65c3a807cb
JoannaCode <chenshuna2012@gmail.com>
2016-04-11 22:00:39 -0700

    finished 146

--- Hard/LRUCache_shuna.java
+++ Hard/LRUCache_shuna.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * LRU Cache
  * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
