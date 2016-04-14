645b4bd428276cb1076b2f53f4a134379bd731aa
Kiyan <freetymesunkiyan@gmail.com>
2016-04-13 22:07:08 -0700

    initial commit

---
+++ Medium/MinSizeSubarraySum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/**
+ * Given an array of n positive integers and a positive integer s, find the 
+ * minimal length of a subarray of which the sum ≥ s. If there isn't one, return 
+ * 0 instead.
+ * 
+ * For example, given the array [2,3,1,2,4,3] and s = 7,
+ * the subarray [4,3] has the minimal length under the problem constraint.
+ * 
+ * More practice:
+ * If you have figured out the O(n) solution, try coding another solution of 
+ * which the time complexity is O(n log n).
+ * 
+ * Tags: Array, Two Pointers, Binary Search
+ *
+ * Similar Problems: (H) Minimum Window Substring, (E) Maximum Size Subarray Sum 
+ *                  Equals k
+ */
+class MinSizeSubarraySum {
+    public int minSubArrayLen(int s, int[] nums) {
+        
+    }
+}
