d06ae2e2231df98d5e138755e03aa4f2f4872220
JoannaCode <chenshuna2012@gmail.com>
2016-04-14 21:43:01 -0700

    fixed 121

--- Easy/BestTimetoBuyandSellStock1.java
+++ Easy/BestTimetoBuyandSellStock1.java
@@ -10,12 +10,12 @@
 
 public class BestTimetoBuyandSellStock {
     public static int maxProfit(int[] prices) {
-        if(prices == null || prices.length == 0 || prices.length == 1){
+        if(prices == null || prices.length < 2){
             return 0;
         }
         int max = 0;
         int minPrice = prices[0];
-        for(int i = 0; i < prices.length; i++){
+        for(int i = 1; i < prices.length; i++){
             minPrice = Math.min(minPrice, prices[i]);
             max = Math.max(max,  prices[i] - minPrice);
         }
