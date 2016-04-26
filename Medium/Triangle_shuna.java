fc7655da822002b650f8eff85efb606056026447
JoannaCode <chenshuna2012@gmail.com>
2016-04-25 20:29:31 -0700

    add botton up solution

--- Medium/Triangle_shuna.java
+++ Medium/Triangle_shuna.java
@@ -19,6 +19,14 @@ import java.util.List;
  */ 
 
 public class Triangle {
+    public static int minimumTotalBottonUp(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; i--)
+            for (int j = 0; j < triangle.get(i).size(); j++)
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+        return dp[0];
+    }
+    
     public static int minimumTotal(List<List<Integer>> triangle) {
         int minSum = Integer.MAX_VALUE;
         int[] sum = new int[triangle.size()];
