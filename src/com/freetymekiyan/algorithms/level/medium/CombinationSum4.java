0748e312fad81d1d8c8a7ec801c8a27969e20cf6
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-07-26 11:13:46 -0700

    update Combination Sum 4 with bottom-up approach

--- src/com/freetymekiyan/algorithms/level/medium/CombinationSum4.java
+++ src/com/freetymekiyan/algorithms/level/medium/CombinationSum4.java
@@ -40,37 +40,44 @@ public class CombinationSum4 {
 
     private CombinationSum4 c;
 
-private int[] dp;
+    private int[] dp;
 
-/**
- *
- * @param nums
- * @param target
- * @return
- */
-public int combinationSum4(int[] nums, int target) {
-    dp = new int[target + 1];
-    Arrays.fill(dp, -1);
-    dp[0] = 1;
-    return helper(nums, target);
-}
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return helper(nums, target);
+    }
 
-private int helper(int[] nums, int target) {
-    if (target == 0) {
-        return 1;
-    }
-    if (dp[target] != -1) {
-        return dp[target];
-    }
-    int res = 0;
-    for (int i = 0; i < nums.length; i++) {
-        if (target >= nums[i]) {
-            res += helper(nums, target - nums[i]);
+    private int helper(int[] nums, int target) {
+        if (target == 0) {
+            return 1;
         }
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) {
+                res += helper(nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return res;
     }
-    dp[target] = res;
-    return res;
-}
+
+    public int combinationSum4BottomUp(int[] nums, int target) {
+        int[] comb = new int[target + 1];
+        comb[0] = 1;
+        for (int i = 1; i < comb.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    comb[i] += comb[i - nums[j]];
+                }
+            }
+        }
+        return comb[target];
+    }
 
     @Before
     public void setUp() {
