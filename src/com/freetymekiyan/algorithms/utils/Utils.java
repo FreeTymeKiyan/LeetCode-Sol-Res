07f057fbe9a337c4075905db80d91552eadaf28c
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-08-29 22:22:37 -0700

    update Utils

--- src/com/freetymekiyan/algorithms/utils/Utils.java
+++ src/com/freetymekiyan/algorithms/utils/Utils.java
@@ -1,5 +1,9 @@
 package com.freetymekiyan.algorithms.utils;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * Utility class.
  */
@@ -13,6 +17,29 @@ public class Utils {
             cur = cur.next;
         }
         return head;
+    }
+
+    public static TreeNode buildBinaryTree(Integer[] values) {
+        if (values == null || values.length == 0) {
+            throw new InvalidParameterException("values should not be null or empty");
+        }
+        TreeNode root = new TreeNode(values[0]);
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int i = 1;
+        while (!queue.isEmpty() && i < values.length) {
+            TreeNode node = queue.poll();
+            if (i < values.length && values[i] != null) {
+                node.left = new TreeNode(values[i]);
+                queue.offer(node.left);
+            }
+            if (i + 1 < values.length && values[i + 1] != null) {
+                node.right = new TreeNode(values[i + 1]);
+                queue.offer(node.right);
+            }
+            i += 2;
+        }
+        return root;
     }
 
     public static class ListNode {
