4854cd90b2712bb21b9b826b2f276baee4d501b8
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-05 17:52:33 -0700

    update decode ways

--- src/com/freetymekiyan/algorithms/level/Medium/DecodeWays.java
+++ src/com/freetymekiyan/algorithms/level/Medium/DecodeWays.java
@@ -1,34 +1,67 @@
+package com.freetymekiyan.algorithms.level.medium;
+
 /**
- * A message containing letters from A-Z is being encoded to numbers using the
- * following mapping:
- * 
+ * A message containing letters from A-Z is being encoded to numbers using the following mapping:
+ * <p>
  * 'A' -> 1
  * 'B' -> 2
  * ...
  * 'Z' -> 26
- * 
- * Given an encoded message containing digits, determine the total number of
- * ways to decode it.
- * 
+ * Given an encoded message containing digits, determine the total number of ways to decode it.
+ * <p>
  * For example,
  * Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
- * 
+ * <p>
  * The number of ways decoding "12" is 2.
- * 
- * Tags: DP, String
+ * <p>
+ * Company Tags: Microsoft, Uber, Facebook
+ * Tags: Dynamic Programming, String
  */
 class DecodeWays {
+
     public static void main(String[] args) {
         DecodeWays d = new DecodeWays();
         System.out.println(d.numDecodings("1029"));
     }
-    
+
     /**
-     * Optimal, DP
-     * Reduce space to O(1)
+     * DP. O(n) Time, O(n) Space.
+     * Suppose the length of the String is n.
+     * Recurrence relation:
+     * number of decode ways at length n related to number of decode ways at previous lengths.
+     * If we can decode the last digit, then ways[n] += ways[n-1].
+     * If we can decode the last two digits, then ways[n] += ways[n-2].
+     * Base case:
+     * ways[0] = 1, means only one way when string length is zero.
+     * ways[1] = 0 or 1, depending on the only digit is 0 or non-zero.
      */
-    public int numDecodingsOptimal(String s) {
-        if (s == null || s.length() == 0) return 0;
+    public static int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+        int[] ways = new int[len + 1];
+        // Base cases
+        ways[0] = 1;
+        ways[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= len; i++) {
+            int code1 = Integer.valueOf(s.substring(i - 1, i));
+            int code2 = Integer.valueOf(s.substring(i - 2, i));
+            // System.out.println(code2);
+            ways[i] = (code1 != 0 ? ways[i - 1] : 0) + (code2 <= 26 && code2 > 9 ? ways[i - 2] : 0);
+        }
+        return ways[len];
+    }
+
+    /**
+     * DP. O(n) Time, O(1) Space.
+     * Don't need an array since we only need previous two results.
+     * Remember to update those results.
+     */
+    public int numDecodingsB(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
         int len = s.length();
         int prev1 = 1;
         int prev2 = s.charAt(0) == '0' ? 0 : 1;
@@ -40,24 +73,5 @@ class DecodeWays {
             prev1 = temp;
         }
         return prev2;
-    }
-    
-    /**
-     * Time O(n), Space O(n)
-     * note that there can be zeros in s
-     */
-    public static int numDecodings(String s) {
-        if (s == null || s.length() == 0) return 0;
-        int len = s.length();
-        int[] ways = new int[len + 1];
-        ways[0] = 1;
-        ways[1] = s.charAt(0) == '0' ? 0 : 1;
-        for (int i = 2; i <= len; i++) {
-            int code1 = Integer.valueOf(s.substring(i - 1, i));
-            int code2 = Integer.valueOf(s.substring(i - 2, i));
-            // System.out.println(code2);
-            ways[i] = (code1 != 0 ? ways[i - 1] : 0) + (code2 <= 26 && code2 > 9 ? ways[i - 2] : 0);
-        }
-        return ways[len];
     }
 }
