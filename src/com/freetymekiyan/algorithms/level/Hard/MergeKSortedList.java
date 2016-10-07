b4d23098bc0b9d79c01d716674fc6e960c6c50be
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-07 15:39:28 -0700

    update merge k sorted lists

--- src/com/freetymekiyan/algorithms/level/Hard/MergeKSortedList.java
+++ src/com/freetymekiyan/algorithms/level/Hard/MergeKSortedList.java
@@ -1,57 +1,94 @@
-import java.util.*;
+package com.freetymekiyan.algorithms.level.hard;
+
+import com.freetymekiyan.algorithms.utils.Utils.ListNode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
- * Merge k sorted linked lists and return it as one sorted list. Analyze and
- * describe its complexity.
- * 
- * Tags: Divide and Conquer, Linkedlist, Heap
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ * <p>
+ * Company Tags: LinkedIn, Google, Uber, Airbnb, Facebook, Twitter, Amazon, Microsoft
+ * Tags: Divide and Conquer, Linked List, Heap
+ * Similar Problems: (E) Merge Two Sorted Lists, (M) Ugly Number II
  */
-class MergeKSortedList {
-    public static void main(String[] args) {
-        
-    }
-    
+public class MergeKSortedList {
+
     /**
-     * Use a heap, O(n * log(k))
+     * Heap. O(k) + O(n * log(k)) Time, O(k) Space.
+     * Keep track of all heads in a heap, so that we know the next value to be inserted in O(log(k)) time.
+     * Create a result list head, and poll from the queue to get the next value.
+     * Keep track of the list's tail with a pointer.
+     * Add the next of what we polled from queue, if it's not null.
      */
-    public ListNode mergeKLists(List<ListNode> lists) {
-        if (lists == null || lists.size() == 0) return null;
-        // Build priority queue
-        Queue<ListNode> queue = new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode n1, ListNode n2) {
-                return n1.val - n2.val;
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        Queue<ListNode> pq = new PriorityQueue<>(lists.length, (n1, n2) -> n1.val - n2.val);
+        for (ListNode n : lists) {
+            if (n != null) {
+                pq.add(n);
             }
-        });
-        for (ListNode n : lists) if (n != null) queue.add(n);
-        
-        ListNode dummy = new ListNode(0); // set dummy head
+        }
+        ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-        while (!queue.isEmpty()) { // build next
-            tail.next = queue.poll();
+        while (!pq.isEmpty()) {
+            tail.next = pq.poll();
             tail = tail.next;
-            if (tail.next != null) queue.add(tail.next);
+            if (tail.next != null) {
+                pq.add(tail.next);
+            }
         }
         return dummy.next;
     }
-    
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
-    }
-    
+
     /**
-     * Divide and conquer
-     * merge two halves, divide to merge two lists
+     * Divide and conquer.
+     * Merge k sorted lists can be divided, suppose we have k lists,
+     * 1) Merge the first k / 2 lists
+     * 2) Merge k / 2 + 1 to k lists
+     * Then just implement merge two lists.
+     * Base cases:
+     * 1) If there is no list, return null;
+     * 2) If there is only 1 list, return the head of that list;
+     * 3) If there are only 2 lists, return the merge two list result.
      */
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-        
+    public ListNode mergeKListsB(ListNode[] lists) {
+        return mergeKListsB(lists, 0, lists.length - 1);
+    }
+
+    public ListNode mergeKListsB(ListNode[] lists, int s, int e) {
+        // Base cases
+        if (s > e) {
+            return null;
+        }
+        if (s == e) {
+            return lists[s];
+        }
+        if (s == e - 1) {
+            return mergeTwoLists(lists[s], lists[s + 1]);
+        }
+        // Merge two halves
+        return mergeTwoLists(mergeKListsB(lists, s, s + (e - s) / 2),
+                             mergeKListsB(lists, s + (e - s) / 2 + 1, e));
+    }
+
+    /**
+     * Recursive.
+     * Get the smaller value of the two lists' heads.
+     * The final merged result should be the smaller node concatenate with the merge result of the other nodes.
+     * For example, suppose l1 is smaller.
+     * Then the result is l1's head + mergeTwoLists(l1.next, l2).
+     */
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
         // next node should be the result of comparison
         if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2); // notice l1.next
@@ -62,14 +99,5 @@ class MergeKSortedList {
         }
     }
 
-    public ListNode mergeKLists(List<ListNode> lists) {
-        /*base cases*/
-        if (lists.size() == 0) return null;
-        if (lists.size() == 1) return lists.get(0);
-        if (lists.size() == 2) return mergeTwoLists(lists.get(0), lists.get(1));
-        /*merge two halves*/
-        return mergeTwoLists(mergeKLists(lists.subList(0, lists.size()/2)), 
-            mergeKLists(lists.subList(lists.size()/2, lists.size())));
-    }
-   
+
 }
