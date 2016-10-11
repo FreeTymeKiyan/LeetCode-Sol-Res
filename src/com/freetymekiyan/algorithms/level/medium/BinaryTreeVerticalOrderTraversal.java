324d01aa4be4f0ffbf83bcf5288fa418e627b09e
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-10 22:30:55 -0700

    add binary tree vertical order traversal

---
+++ src/com/freetymekiyan/algorithms/level/medium/BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,122 @@
+package com.freetymekiyan.algorithms.level.medium;
+
+import com.freetymekiyan.algorithms.utils.Utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by
+ * column).
+ * <p>
+ * If two nodes are in the same row and column, the order should be from left to right.
+ * <p>
+ * Examples:
+ * <p>
+ * Given binary tree [3,9,20,null,null,15,7],
+ * |   3
+ * |  /\
+ * | /  \
+ * | 9  20
+ * |    /\
+ * |   /  \
+ * |  15   7
+ * return its vertical order traversal as:
+ * |[
+ * |  [9],
+ * |  [3,15],
+ * |  [20],
+ * |  [7]
+ * |]
+ * Given binary tree [3,9,8,4,0,1,7],
+ * |     3
+ * |    /\
+ * |   /  \
+ * |   9   8
+ * |  /\  /\
+ * | /  \/  \
+ * | 4  01   7
+ * return its vertical order traversal as:
+ * |[
+ * |  [4],
+ * |  [9],
+ * |  [3,0,1],
+ * |  [8],
+ * |  [7]
+ * |]
+ * Given binary tree [3,9,8,4,0,1,7,null,null,null,2,5] (0's right child is 2 and 1's left child is 5),
+ * |     3
+ * |    /\
+ * |   /  \
+ * |   9   8
+ * |  /\  /\
+ * | /  \/  \
+ * | 4  01   7
+ * |    /\
+ * |   /  \
+ * |   5   2
+ * return its vertical order traversal as:
+ * |[
+ * |  [4],
+ * |  [9,5],
+ * |  [3,0,1],
+ * |  [8,2],
+ * |  [7]
+ * |]
+ * Company Tags: Google, Snapchat, Facebook
+ * Tags: Hash Table
+ * Similar Problems: (E) Binary Tree Level Order Traversal
+ */
+public class BinaryTreeVerticalOrderTraversal {
+
+    /**
+     * BFS, Hash Table.
+     * Use a map to record level and relative node values.
+     * Initialize level value of root as 0.
+     * When accessing a left child, col value decrement by 1.
+     * When accessing right, col value increment by 1.
+     * Use a separate queue to store column.
+     */
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> cols = new LinkedList<>();
+        queue.add(root);
+        cols.add(0);
+        int min = 0;
+        int max = 0;
+
+        while (!queue.isEmpty()) {
+            TreeNode n = queue.poll();
+            int level = cols.poll();
+            if (!map.containsKey(level)) {
+                map.put(level, new ArrayList<>());
+            }
+            map.get(level).add(n.val);
+            if (n.left != null) {
+                queue.add(n.left);
+                cols.add(level - 1);
+                min = Math.min(min, level - 1);
+            }
+            if (n.right != null) {
+                queue.add(n.right);
+                cols.add(level + 1);
+                max = Math.max(max, level + 1);
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            res.add(map.get(i));
+        }
+        return res;
+    }
+
+}
