7f67cdf5c1e9ca2671122abdf75fe7ef89005d81
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-15 13:01:03 -0700

    update Remove Invalid Parentheses

--- src/com/freetymekiyan/algorithms/level/Hard/RemoveInvalidParentheses.java
+++ src/com/freetymekiyan/algorithms/level/Hard/RemoveInvalidParentheses.java
@@ -1,3 +1,5 @@
+package com.freetymekiyan.algorithms.level.hard;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -8,58 +10,62 @@ import java.util.Set;
 /**
  * Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible
  * results.
- *
+ * <p>
  * Note: The input string may contain letters other than the parentheses ( and ).
- *
+ * <p>
  * Examples:
- *
  * "()())()" -> ["()()()", "(())()"]
- *
  * "(a)())()" -> ["(a)()()", "(a())()"]
- *
  * ")(" -> [""]
- *
+ * <p>
+ * Company Tags: Facebook
  * Tags: Depth-first Search, Breadth-first Search
- *
  * Similar Problems: (E) Valid Parentheses
  */
 public class RemoveInvalidParentheses {
 
     /**
-     * BFS check all possible state of each level
+     * BFS.
+     * Take the string after removing one parentheses as a level.
+     * First add the original string given to a queue.
+     * Then start BFS, each time poll a string from the queue.
+     * Check if the polled string is valid, if its valid, we don't need to generate next level.
+     * If it's not, generate all possible strings for the next level by removing one parentheses.
+     * <p>
+     * Note that once we found one valid string, we know the minimum number.
+     * No need to generate the next level anymore.
+     * We can also use a String set to avoid duplicate check on strings.
      */
     public List<String> removeInvalidParentheses(String s) {
         List<String> res = new ArrayList<>();
         if (s == null) {
             return res;
         }
-
+        // BFS
         Set<String> visited = new HashSet<>();
         Queue<String> queue = new LinkedList<>();
-
         queue.add(s);
         visited.add(s);
         boolean found = false;
-
         while (!queue.isEmpty()) {
-            s = queue.poll();
+            String t = queue.poll();
 
-            if (isValid(s)) {
-                res.add(s);
+            if (isValid(t)) {
+                res.add(t);
                 found = true;
             }
 
-            if (found) {
+            if (found) { // No need to generate the next level.
                 continue;
             }
 
-            // generate all possible states for next level
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) != '(' && s.charAt(i) != ')') {
+            // Generate all possible states for next level
+            for (int i = 0; i < t.length(); i++) {
+                if (t.charAt(i) != '(' && t.charAt(i) != ')') { // Skip other chars
                     continue;
                 }
 
-                String t = s.substring(0, i) + s.substring(i + 1);
+                t = t.substring(0, i) + t.substring(i + 1); // If i + 1 == t.length(), t.substring(i + 1) will be ""
 
                 if (!visited.contains(t)) {
                     queue.add(t);
@@ -70,6 +76,16 @@ public class RemoveInvalidParentheses {
         return res;
     }
 
+    /**
+     * Use an integer counter as a Stack.
+     * Make sure there are more left parens than right ones.
+     * Increase the counter when there is a left paren.
+     * If there is a right paren, check whether there is enough paren to match first.
+     * If counter is zero, which means no left paren available, return false;
+     * Otherwise decrement counter by 1.
+     * Return true if counter is 0 that is fully matched.
+     * Otherwise false.
+     */
     private boolean isValid(String s) {
         int count = 0;
 
@@ -77,8 +93,7 @@ public class RemoveInvalidParentheses {
             char c = s.charAt(i);
             if (c == '(') {
                 count++;
-            }
-            if (c == ')' && count-- == 0) {
+            } else if (c == ')' && count-- == 0) {
                 return false;
             }
         }
@@ -87,7 +102,7 @@ public class RemoveInvalidParentheses {
     }
 
     /**
-     * DFS
+     * DFS.
      * https://leetcode.com/discuss/81478/easy-short-concise-and-fast-java-dfs-3-ms-solution
      */
     public List<String> removeInvalidParentheses2(String s) {
