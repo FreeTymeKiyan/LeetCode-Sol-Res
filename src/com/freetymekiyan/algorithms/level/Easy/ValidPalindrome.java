a7893abde8a57e4127ca25f9cbc7329e28351862
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-16 15:06:23 -0700

    update Valid Palindrome

--- src/com/freetymekiyan/algorithms/level/Easy/ValidPalindrome.java
+++ src/com/freetymekiyan/algorithms/level/Easy/ValidPalindrome.java
@@ -1,67 +1,80 @@
+package com.freetymekiyan.algorithms.level.easy;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
- * Given a string, determine if it is a palindrome, considering only
- * alphanumeric characters and ignoring cases.
- *
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ * <p>
  * For example,
  * "A man, a plan, a canal: Panama" is a palindrome.
  * "race a car" is not a palindrome.
- *
+ * <p>
  * Note:
- * Have you consider that the string might be empty? This is a good question to
- * ask during an interview.
- *
+ * Have you consider that the string might be empty? This is a good question to ask during an interview.
+ * <p>
  * For the purpose of this problem, we define empty string as valid palindrome.
- *
- * Tags: Two pointers, String
+ * <p>
+ * Company Tags: Microsoft, Uber, Facebook, Zenefits
+ * Tags: Two Pointers, String
+ * Similar Problems: (E) Palindrome Linked List
  */
-class ValidPalindrome {
-    public static void main(String[] args) {
-        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
-    }
+public class ValidPalindrome {
+
+    private ValidPalindrome v;
 
     /**
-     * Two pointers
-     * what characters do we have for input? space?
-     * case sensitive or not?
+     * Two pointers.
+     * Ask for clarification:
+     * What characters do we have for input? Space?
+     * Case sensitive or not?
+     * First convert the string to lowercase.
+     * Then start from both ends and move pointers to the next alphanumeric character.
+     * If these characters are not the same, return false.
+     * Remember to move pointers one more step for the next round.
      */
-    public static boolean isPalindromeB(String s) {
-        if (s == null || s.length() == 0) return true;
-
-        int start = 0;
-        int end = s.length() - 1;
-        s = s.toLowerCase();
-        while (start < end) {
-            // move start
-            while (start <= end && !isValid(s.charAt(start))) start++;
-            // for string without letters or digit
-            if (start > end) return true;
-            // move end
-            while (start <= end && !isValid(s.charAt(end))) end--;
-            if (s.charAt(start) != s.charAt(end)) break;
-            start++;
-            end--;
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
         }
-        return end <= start;
-    }
-
-    private static boolean isValid(char c) {
-        return Character.isLetterOrDigit(c);
-    }
-
-    /**
-     * Put letters and numbers in a new string and convert to lowercase
-     * O(2n) Time, O(n) Space
-     */
-    public static boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) if (Character.isLetterOrDigit(c)) sb.append(c);
-        String copy = sb.toString().toLowerCase();
-        int length = copy.length();
-        for (int i = 0; i < length / 2; i++) {
-            if (copy.charAt(i) != copy.charAt(length - (i + 1))) {
+        int i = 0;
+        int j = s.length() - 1;
+        s = s.toLowerCase();
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            if (i >= j) {
+                return true;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
+
+    @Before
+    public void setUp() {
+        v = new ValidPalindrome();
+    }
+
+    @Test
+    public void testExamples() {
+        Assert.assertTrue(v.isPalindrome("A man, a plan, a canal: Panama"));
+        Assert.assertFalse(v.isPalindrome("race a car"));
+    }
+
+    @After
+    public void tearDown() {
+        v = null;
+    }
+
 }
