85dcfb55bc35b40aebf009d506691014a8b8aaae
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-16 23:12:18 -0700

    update Insert Interval

--- src/com/freetymekiyan/algorithms/level/Hard/InsertInterval.java
+++ src/com/freetymekiyan/algorithms/level/Hard/InsertInterval.java
@@ -1,87 +1,94 @@
-import java.util.*;
+package com.freetymekiyan.algorithms.level.hard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Given a set of non-overlapping intervals, insert a new interval into the
- * intervals (merge if necessary).
- * 
- * You may assume that the intervals were initially sorted according to their
- * start times.
- * 
+ * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+ * <p>
+ * You may assume that the intervals were initially sorted according to their start times.
+ * <p>
  * Example 1:
  * Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
- * 
+ * <p>
  * Example 2:
- * Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as
- * [1,2],[3,10],[12,16].
- * 
+ * Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+ * <p>
  * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
- * 
+ * <p>
+ * Company Tags: LinkedIn, Google, Facebook
  * Tags: Array, Sort
+ * Similar Problems: (H) Merge Intervals
  */
-class InsertInterval {
-    public static void main(String[] args) {
-        
-    }
-    
+public class InsertInterval {
+
     /**
-     * O(n), not in place solution, make use of intervals are sorted
-     * Go through the list, compare interval's start and end with the last 
-     * interval of result, they may overlap
+     * Array, Sort. O(n).
+     * Not in place solution, make use of intervals are sorted.
+     * Go through the given intervals.
+     * For each of them, there are 3 situations:
+     * 1) No overlap, in front of the new interval
+     * 2) No overlap, behind the new interval
+     * 3) Overlap, merge with the new interval
      */
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        List<Interval> res = new ArrayList<Interval>();
+        List<Interval> res = new ArrayList<>();
         res.add(newInterval);
-        if (intervals == null || intervals.size() == 0) return res;
+        if (intervals == null || intervals.size() == 0) {
+            return res;
+        }
         for (Interval i : intervals) {
-            int a = res.get(res.size() - 1).start;
-            int b = res.get(res.size() - 1).end;
-            if (i.end < a) res.add(res.size() - 1, i); // no overlap, add to second last
-            else if (b < i.start) res.add(i); // no overlap, add to last
-            else {
-                a = Math.min(a, i.start);
-                b = Math.max(b, i.end);
-                res.set(res.size()- 1, new Interval(a, b));
+            int start = res.get(res.size() - 1).start;
+            int end = res.get(res.size() - 1).end;
+            if (i.end < start) {
+                res.add(res.size() - 1, i); // No overlap, insert to second last
+            } else if (end < i.start) {
+                res.add(i); // No overlap, append to last
+            } else {
+                start = Math.min(start, i.start);
+                end = Math.max(end, i.end);
+                res.set(res.size() - 1, new Interval(start, end));
             }
         }
         return res;
     }
-    
+
     /**
-     * In place solution
-     * Find start and end point of the interval to be merged
+     * In place solution.
+     * Skip the intervals not overlapping with the new one.
+     * Merge all intervals overlap with the new interval.
+     * And remove them.
+     * Add new interval in position.
      */
-    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        List<Interval> results = new ArrayList<Interval>();
-        if (intervals == null||intervals.size() == 0){
-            results.add(newInterval);
-            return results;
+    public List<Interval> insertB(List<Interval> intervals, Interval newInterval) {
+        int i = 0;
+        // Skip those intervals with no overlap with new interval
+        while (i < intervals.size() && intervals.get(i).end < newInterval.start) {
+            i++;
         }
-        // find position for new interval
-        int start = 0, end = 0; // end points to the position after
-        for (Interval i : intervals){
-            if (newInterval.start > i.end) start++;
-            if (newInterval.end >= i.start) end++;
-            else break;
+        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval =
+                new Interval(Math.min(intervals.get(i).start, newInterval.start),
+                             Math.max(intervals.get(i).end, newInterval.end));
+            intervals.remove(i);
         }
-        if (start == end) {
-            results.addAll(intervals);
-            results.add(start, newInterval);
-            return results;
-        }
-        // add intervals from 0 to start
-        for (int i = 0; i < start; i++) results.add(intervals.get(i));
-        // build and add overlapped interval
-        Interval interval = new Interval(Math.min(newInterval.start, intervals.get(start).start), Math.max(newInterval.end, intervals.get(end - 1).end)); // note that it's end - 1
-        results.add(interval);
-        // add remainning intervals
-        for (int i = end; i < intervals.size(); i++) results.add(intervals.get(i));
-        return results;
+        intervals.add(i, newInterval);
+        return intervals;
     }
-    
+
     public class Interval {
+
         int start;
         int end;
-        Interval() { start = 0; end = 0; }
-        Interval(int s, int e) { start = s; end = e; }
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
     }
 }
