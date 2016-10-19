58c677924c4b2ddf8d96f37ca6be6fd42e3b67fd
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-18 23:54:24 -0700

    update Longest Consecutive Sequence

--- src/com/freetymekiyan/algorithms/level/Hard/LongestConsecutiveSeq.java
+++ src/com/freetymekiyan/algorithms/level/Hard/LongestConsecutiveSeq.java
@@ -1,49 +1,59 @@
-import java.util.*;
+package com.freetymekiyan.algorithms.level.hard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Given an unsorted array of integers, find the length of the longest
- * consecutive elements sequence.
- * 
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ * <p>
  * For example,
  * Given [100, 4, 200, 1, 3, 2],
- * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its
- * length: 4.
- * 
+ * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+ * <p>
  * Your algorithm should run in O(n) complexity.
- * 
- * Tags: Array, HashTable
+ * <p>
+ * Company Tags: Google, Facebook
+ * Tags: Array, Union Find
+ * Similar Problems: (M) Binary Tree Longest Consecutive Sequence
  */
-class LongestConsecutiveSeq {
-    public static void main(String[] args) {
-        LongestConsecutiveSeq l = new LongestConsecutiveSeq();
-        int[] a = {100, 4, 200, 1, 3, 2};
-        System.out.println(l.longestConsecutive(a));
-    }
+public class LongestConsecutiveSeq {
 
     /**
-     * Use a map to store ranges
-     * Get lower bound with smaller value
-     * Get upper bound with larger value
-     * Update max length with new bound
-     * Put all possible ranges into map
-     * including num[i] ~ num[i], low ~ upp, upp ~ low
+     * Set.
+     * Add numbers into a set first.
+     * Then for each number i:
+     * 1) Make sure it's the start of a streak by check i - 1.
+     * 2) If it's the start, check i + 1, i + 2 ... till reach j where it's not in the set.
+     * Update max length by j - i.
      */
-    public int longestConsecutive(int[] num) {
-        if (num == null || num.length == 0) return 0;
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int maxLen = 0;
-        for(int i = 0; i < num.length; i++) {
-            if (map.containsKey(num[i])) continue; // skip duplicates
-            int low = num[i]; // initialize ranges
-            int upp = num[i];
-            if (map.containsKey(num[i] - 1)) low = map.get(num[i] - 1);
-            if (map.containsKey(num[i] + 1)) upp = map.get(num[i] + 1);
-            maxLen = Math.max(maxLen, upp - low + 1); // update length
-            // put possible ranges into map for next iteration
-            map.put(num[i], num[i]);
-            map.put(low, upp);
-            map.put(upp, low);
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-        return maxLen;
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            set.add(i);
+        }
+        int max = 1;
+        for (int i : set) {
+            if (set.contains(i - 1)) {
+                continue;
+            }
+            int j = i + 1;
+            while (set.contains(j)) {
+                j++;
+            }
+            max = Math.max(j - i, max);
+        }
+        return max;
+    }
+
+    @Test
+    public void testExamples() {
+        int[] a = {100, 4, 200, 1, 3, 2};
+        Assert.assertEquals(4, longestConsecutive(a));
     }
 }
