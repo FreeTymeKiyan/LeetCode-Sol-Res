396b0ff8673a56b2fb23d53d303a9d8fb9d03865
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-18 21:58:20 -0700

    update Inorder Successor In BST

--- src/com/freetymekiyan/algorithms/level/medium/InorderSuccessorInBST.java
+++ src/com/freetymekiyan/algorithms/level/medium/InorderSuccessorInBST.java
@@ -17,9 +17,9 @@ public class InorderSuccessorInBST {
      * Tree, recursive.
      * In BST, root is the largest of left subtree, smallest of right subtree.
      * In-order successor is the smallest of all node's larger than p.
+     * Then we can do a binary search like recurse to search the result.
      * <p>
-     * If root's val == p's, p's inorder successor is in the right subtree.
-     * If root's val <= p's, p's inorder successor is in the right subtree, still.
+     * If root's val <= p's, p's inorder successor is in the right subtree.
      * If root's val > p's, p's inorder successor can be in the left subtree.
      * Or is root itself if p is the largest in left subtree.
      */
@@ -31,6 +31,7 @@ public class InorderSuccessorInBST {
             return inorderSuccessor(root.right, p);
         } else {
             TreeNode succ = inorderSuccessor(root.left, p);
+            // If not found in left subtree, it's root.
             return succ == null ? root : succ;
         }
     }
@@ -41,6 +42,12 @@ public class InorderSuccessorInBST {
      * If p's val >= root's, go to the right subtree.
      */
     public TreeNode inorderSuccessorB(TreeNode root, TreeNode p) {
+        if (p.right != null) { // Pruning, if p has right subtree.
+            for (TreeNode cur = p.right; cur != null; cur = cur.left) {
+                p = cur; // Successor will be leftmost leaf of right subtree.
+            }
+            return p;
+        }
         TreeNode succ = null;
         while (root != null) {
             if (p.val < root.val) {
