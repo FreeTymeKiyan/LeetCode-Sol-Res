daeb493ddf41f9c7fd7838f8fcafbfce4fd38364
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-10-30 18:47:41 -0700

    update House Robber

--- src/com/freetymekiyan/algorithms/level/Easy/HouseRobber.java
+++ src/com/freetymekiyan/algorithms/level/Easy/HouseRobber.java
@@ -1,3 +1,5 @@
+package com.freetymekiyan.algorithms.level.easy;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,12 +10,12 @@ import org.junit.Test;
  * stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system
  * connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
  * <p>
- * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount
- * of money you can rob tonight without alerting the police.
+ * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of
+ * money you can rob tonight without alerting the police.
  * <p>
+ * Company Tags: LinkedIn, Airbnb
  * Tags: Dynamic Programming
- * <p>
- * Similar Problems: (M) Maximum Product Subarray (M) House Robber II (M) Paint House (E) Paint Fence (M) House
+ * Similar Problems: (M) Maximum Product Subarray, (M) House Robber II, (M) Paint House, (E) Paint Fence, (M) House
  * Robber III
  */
 public class HouseRobber {
@@ -21,18 +23,41 @@ public class HouseRobber {
     private HouseRobber hr;
 
     /**
-     * Recurrence relation: max[n] = max(max[n - 2] + nums[n], max[n - 1])
-     * Optimization: use constant variables instead of an array
+     * DP. Space Optimized.
+     * Max amount of house n is either rob the previous house or rob this house.
+     * Recurrence relation:
+     * max[n] = max(max[n - 2] + nums[n], max[n - 1])
+     * Optimization:
+     * Use constant variables instead of an array.
      */
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
         int preMax = nums[0];
         int max = Math.max(nums[0], nums[1]);
         for (int i = 2; i < nums.length; i++) {
             int temp = preMax;
             preMax = max;
             max = Math.max(temp + nums[i], preMax);
+        }
+        return max;
+    }
+
+    /**
+     * DP.
+     * More compact code.
+     */
+    public int robB(int[] nums) {
+        int preMax = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int curMax = Math.max(preMax + nums[i], max);
+            preMax = max;
+            max = curMax;
         }
         return max;
     }
---
+++ src/com/freetymekiyan/algorithms/level/easy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+/**
+ * Given a binary tree, find its maximum depth.
+ * <p>
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * <p>
+ * Tags: Tree, DFS
+ */
+public class MaxDepth {
+
+    /**
+     * Recursive, O(n)
+     * If tree is empty, return 0
+     * Else
+     * Get the max depth of left subtree recursively
+     * Get the max depth of right subtree recursively
+     * Get the max of max depths of left and right subtrees and add 1 to it
+     */
+    private int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+}
