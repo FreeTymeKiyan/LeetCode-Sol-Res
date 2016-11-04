c887852b85eca1db193f043c2c53e36b128c059b
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-03 21:55:33 -0700

    update Three Sum

--- src/com/freetymekiyan/algorithms/level/Medium/ThreeSum.java
+++ src/com/freetymekiyan/algorithms/level/Medium/ThreeSum.java
@@ -84,24 +84,28 @@ class ThreeSum {
         Arrays.sort(num);
 
         for (int i = 0; i < num.length - 2; i++) {
-            if (num[i] > 0) {
+            if (num[i] > 0) { // Pruning.
                 break;
             }
             if (i > 0 && num[i] == num[i - 1]) {
                 continue;
             }
-            // Two sum.
+            // Two sum search.
             int lo = i + 1, hi = num.length - 1, sum = 0 - num[i];
+            if (num[i] + num[lo] > 0) { // Pruning.
+                break;
+            }
             while (lo < hi) {
                 if (num[lo] + num[hi] == sum) {
                     res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                    // Update pointers and skip duplicates.
+                    // Skip duplicates.
                     while (lo < hi && num[lo] == num[lo + 1]) {
                         lo++;
                     }
                     while (lo < hi && num[hi] == num[hi - 1]) {
                         hi--;
                     }
+                    // Update pointers. Must happen after duplicates are all skipped.
                     lo++;
                     hi--;
                 } else if (num[lo] + num[hi] < sum) {
