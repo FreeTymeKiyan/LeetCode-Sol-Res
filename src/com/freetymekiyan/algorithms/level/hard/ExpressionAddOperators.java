d266e432ddfeb41d20ac34b45331eef52fdf9684
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-05 12:56:56 -0700

    update expression add operators

--- src/com/freetymekiyan/algorithms/level/hard/ExpressionAddOperators.java
+++ src/com/freetymekiyan/algorithms/level/hard/ExpressionAddOperators.java
@@ -26,65 +26,71 @@ public class ExpressionAddOperators {
      */
     public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<>();
-        if (num == null || num.length() == 0) {
-            return res;
-        }
-        backtrack(res, num, target, 0, "", 0, 0);
+        backtrack(res, num.toCharArray(), target, 0, new StringBuilder(), 0, 0);
         return res;
     }
 
     /**
      * Backtracking.
-     * What kind of data do we need to preserve for between levels?
-     * Of course the original data, the number string and the target.
-     * And the result we are going to generate.
-     * Then the state of each level, like:
+     * What kind of data do we need to preserve for between recursive calls?
+     * Of course the result list we are going to generate.
+     * And the original data: the number string and the target.
+     * Then the states of each call, like:
      * 1. current position in the number string.
      * 2. the formula generated so far.
      * 3. the evaluation result of that formula.
-     * 4. For multiplication, it is higher priority than + or -, so need to remember the previous multiplied.
+     * 4. For multiplication, it is higher priority than + or -, so need to remember the previous multiplied number.
      * <p>
      * Implementation:
      * Stop when we reach the end of number.
-     * If we found target, add the path to result.
+     * If we found target, add the formula to result.
      * For each value start from the starting index, parse it to long.
      * If start is 0, it means the first number. Just add it to path.
      * Else we search "+", "-", "*" one by one.
-     * For path just concatenate the number to path with relative operator.
+     * For formula just concatenate the number to path with relative operator.
      * res, num and target are the same.
-     * Next starting index will increment by 1.
-     * Evaluate result will change.
+     * Next starting position will increment by 1.
+     * Evaluate result will change according to the operator added.
      * Note that for multiplication, the evaluation result should first minus previous multiplied value.
      * Then add the product of that value and current value.
-     * Then multiplied value should also multiply current value.
+     * The multiplied value should also multiply current value.
      *
      * @param res    Result paths.
-     * @param path   Current path.
-     * @param num    Original number string.
+     * @param num    Original number string. Converted to char array for faster speed.
      * @param target The target value to find.
-     * @param start  Starting index in number string.
-     * @param eval   Actual value of the path.
+     * @param pos    Starting index in number string.
+     * @param expr   Current expression.
+     * @param eval   Actual value of the expression.
      * @param multed The value to be multiplied in the next recursion.
      */
-    private void backtrack(List<String> res, String num, int target, int start, String path, long eval, long multed) {
-        if (start == num.length()) { // Reach the end of num.
+    private void backtrack(List<String> res, char[] num, int target, int pos, StringBuilder expr, long eval,
+                           long multed) {
+        if (pos == num.length) { // Reach the end of num.
             if (target == eval) { // Found target.
-                res.add(path);
+                res.add(expr.toString());
             }
             return;
         }
-
-        for (int i = start; i < num.length(); i++) {
-            if (num.charAt(start) == '0' && i != start) { // Avoid multiple digits start with 0.
+        long cur = 0;
+        for (int i = pos; i < num.length; i++) {
+            if (num[pos] == '0' && i != pos) { // Avoid multiple digits start with 0.
                 break;
             }
-            long cur = Long.parseLong(num.substring(start, i + 1)); // Avoid overflow, along with eval and multed.
-            if (start == 0) { // First number
-                backtrack(res, num, target, i + 1, path + cur, cur, cur);
+            cur = 10 * cur + (num[i] - '0'); // Avoid overflow, along with eval and multed.
+            int len = expr.length();
+            if (pos == 0) { // First number.
+                backtrack(res, num, target, i + 1, expr.append(cur), cur, cur);
+                expr.setLength(len); // Reset string builder.
             } else {
-                backtrack(res, num, target, i + 1, path + "+" + cur, eval + cur, cur);
-                backtrack(res, num, target, i + 1, path + "-" + cur, eval - cur, -cur);
-                backtrack(res, num, target, i + 1, path + "*" + cur, eval - multed + multed * cur, multed * cur);
+                backtrack(res, num, target, i + 1, expr.append("+").append(cur), eval + cur, cur);
+                expr.setLength(len);
+                backtrack(res, num, target, i + 1, expr.append("-").append(cur), eval - cur, -cur);
+                expr.setLength(len);
+                // For multiplication, eval needs to subtract previous multed first, then add multed * cur
+                // multed just multiply cur
+                backtrack(res, num, target, i + 1, expr.append("*").append(cur), eval - multed + multed * cur,
+                          multed * cur);
+                expr.setLength(len);
             }
         }
     }
