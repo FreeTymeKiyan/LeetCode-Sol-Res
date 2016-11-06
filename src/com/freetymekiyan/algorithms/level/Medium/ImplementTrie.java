19c8b0bff0962e7e59119b9883ebceffaf530bcc
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-05 22:06:55 -0700

    update implement trie

--- src/com/freetymekiyan/algorithms/level/Medium/ImplementTrie.java
+++ src/com/freetymekiyan/algorithms/level/Medium/ImplementTrie.java
@@ -1,3 +1,5 @@
+package com.freetymekiyan.algorithms.level.medium;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,102 +9,11 @@ import org.junit.Test;
  * Note:
  * You may assume that all inputs are consist of lowercase letters a-z.
  * <p>
+ * Company Tags: Google, Uber, Facebook, Twitter, Microsoft, Bloomberg
  * Tags: Design, Trie
  * Similar Problems: (M) Add and Search Word - Data structure design
  */
 public class ImplementTrie {
-
-    class TrieNode {
-        private final int R = 26;
-        private TrieNode[] links;
-        private boolean isEnd;
-
-        // Initialize your data structure here.
-        public TrieNode() {
-            links = new TrieNode[26];
-        }
-
-        public boolean hasLink(char ch) {
-            return links[ch -'a'] != null;
-        }
-        public TrieNode getNode(char ch) {
-            return links[ch -'a'];
-        }
-        public void putNode(char ch, TrieNode node) {
-            links[ch -'a'] = node;
-        }
-        public void setEnd() {
-            isEnd = true;
-        }
-        public boolean isEnd() {
-            return isEnd;
-        }
-    }
-
-    public class Trie {
-        private TrieNode root;
-        private int size;
-
-        public Trie() {
-            root = new TrieNode();
-        }
-
-        // Inserts a word into the trie.
-        public void insert(String word) {
-            if (word == null) return;
-            TrieNode node = root;
-            for (int i = 0; i < word.length(); i++) {
-                char currentChar = word.charAt(i);
-                if (!node.hasLink(currentChar)) {
-                    node.putNode(currentChar, new TrieNode());
-                }
-                node = node.getNode(currentChar);
-            }
-            node.setEnd();
-            size++;
-        }
-
-        // search a prefix or whole key in trie and
-        // returns the node where search ends
-        private TrieNode searchPrefix(String word) {
-            TrieNode node = root;
-            for (int i = 0; i < word.length(); i++) {
-                char currentChar = word.charAt(i);
-                if (node.hasLink(currentChar)) {
-                    node = node.getNode(currentChar);
-                } else {
-                    return null;
-                }
-            }
-            return node;
-        }
-
-        // Returns if the word is in the trie.
-        public boolean search(String word) {
-            if (word == null) return false;
-            TrieNode node = searchPrefix(word);
-            return node != null && node.isEnd();
-        }
-
-        // Returns if there is any word in the trie
-        // that starts with the given prefix.
-        public boolean startsWith(String prefix) {
-            if (prefix == null) return false;
-            TrieNode node = searchPrefix(prefix);
-            return node != null;
-        }
-    }
-
-    @Test
-    public void testInsert() {
-        Trie trie = new Trie();
-        trie.insert(null);
-        trie.insert("key");
-        trie.insert("ke");
-        trie.insert("kevin");
-        trie.insert("kiyan");
-        Assert.assertEquals(4, trie.size);
-    }
 
     @Test
     public void testStartsWith() {
@@ -138,6 +49,96 @@ public class ImplementTrie {
         Assert.assertTrue(trie.search("kiyan"));
         trie.insert("ke");
         Assert.assertTrue(trie.search("ke"));
+    }
+
+    class TrieNode {
+
+        private final int R = 26;
+        private TrieNode[] links;
+        private boolean end;
+
+        // Initialize your data structure here.
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean hasLink(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode getNode(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void putNode(char ch) {
+            links[ch - 'a'] = new TrieNode();
+        }
+
+        public void setEnd() {
+            end = true;
+        }
+
+        public boolean isEnd() {
+            return end;
+        }
+    }
+
+    public class Trie {
+
+        private TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        // Inserts a word into the trie.
+        public void insert(String word) {
+            if (word == null) {
+                return;
+            }
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (!node.hasLink(c)) {
+                    node.putNode(c);
+                }
+                node = node.getNode(c);
+            }
+            node.setEnd();
+        }
+
+        // search a prefix or whole key in trie and
+        // returns the node where search ends
+        private TrieNode searchPrefix(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (!node.hasLink(c)) {
+                    return null;
+                }
+                node = node.getNode(c);
+            }
+            return node;
+        }
+
+        // Returns if the word is in the trie.
+        public boolean search(String word) {
+            if (word == null) {
+                return false;
+            }
+            TrieNode node = searchPrefix(word);
+            return node != null && node.isEnd(); // Note to check node.isEnd().
+        }
+
+        // Returns if there is any word in the trie
+        // that starts with the given prefix.
+        public boolean startsWith(String prefix) {
+            if (prefix == null) {
+                return false;
+            }
+            TrieNode node = searchPrefix(prefix);
+            return node != null;
+        }
     }
 
 // Your Trie object will be instantiated and called as such:
