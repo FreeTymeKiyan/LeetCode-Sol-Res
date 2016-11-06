bd6ea1bffcd1a4b99d7ce336714b12e297aee55c
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-06 15:18:27 -0800

    update multiply strings

--- src/com/freetymekiyan/algorithms/level/Medium/MultiplyStrings.java
+++ src/com/freetymekiyan/algorithms/level/Medium/MultiplyStrings.java
@@ -1,48 +1,55 @@
-/**
- * Given two numbers represented as strings, return multiplication of the
- * numbers as a string.
- * 
- * Note: The numbers can be arbitrarily large and are non-negative.
- * 
- * Tags: Math, String
- */
-class MultiplyStrings {
+package com.freetymekiyan.algorithms.level.medium;
 
-    public static void main(String[] args) {
-        
-    }
-    
+/**
+ * Given two numbers represented as strings, return multiplication of the numbers as a string.
+ * <p>
+ * Note:
+ * The numbers can be arbitrarily large and are non-negative.
+ * Converting the input string to integer is NOT allowed.
+ * You should NOT use internal library such as BigInteger.
+ * <p>
+ * Company Tags: Facebook, Twitter
+ * Tags: Math, String
+ * Similar Problems: (M) Add Two Numbers, (E) Plus One, (E) Add Binary, (E) Add Strings
+ */
+public class MultiplyStrings {
+
     /**
-     * Use an integer array to store the result of each digit and build
-     * Convert digit to integer to calculate res of each digit
-     * addRecursive carry to next res, and mod current res with 10
-     * Note that the result digit is reversed in the array
-     * Skip trailing zeros and build from behind
+     * Math, String.
+     * How to do multiplication?
+     * Start from right to left, multiply each pair of digits, and add them together.
+     * Result num1[i] * num2[j] will be placed at i + j and i + j + 1.
+     * Mimic this process.
+     * Special cases:
+     * 1) If one of the strings is null, return empty.
+     * 2) If one of the strings is zero, return zero.
      */
     public String multiply(String num1, String num2) {
-        if (num1 == null || num2 == null) return "";
-        if (num1.equals("0") || num2.equals("0")) return "0"; // if one is 0
-        char[] c1 = num1.toCharArray();
-        char[] c2 = num2.toCharArray();
-        int m = c1.length;
-        int n = c2.length;
-        int[] res = new int[m + n]; // max length possible
+        if (num1 == null || num2 == null) {
+            return "";
+        }
+        if (num1.equals("0") || num2.equals("0")) { // If one number is 0.
+            return "0";
+        }
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n]; // Length is at most m + n.
+        // Pick one digit from one number, multiply with each digit in the other number.
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                // note += below
-                res[m + n - i - j - 2] += (c1[i] - '0') * (c2[j] - '0'); // reverse to keep zeros of result but skip unnecessary zeros
-                res[m + n - i - j - 1] += res[m + n - i - j - 2] / 10;
-                res[m + n - i - j - 2] %= 10;
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1; // p1 is a more significant digit than p2
+                int sum = mul + pos[p2];
+                pos[p1] += sum / 10; // Carry.
+                pos[p2] = sum % 10;
             }
         }
-        // build ans string
+
         StringBuilder ans = new StringBuilder();
-        for (int i = m + n - 1; i >= 0; i--) {
-            if (res[i] != 0) {
-                for (int j = i; j >= 0; j--) ans.append(res[j]); // note res[j]
-                return ans.toString();
+        for (int p : pos) {
+            if (p != 0 || ans.length() != 0) { // Not zero, or not empty.
+                ans.append(p);
             }
         }
-        return "0";
+        return ans.toString();
     }
 }
