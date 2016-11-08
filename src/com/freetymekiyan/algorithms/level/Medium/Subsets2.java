fbc0870c58e7f3a7ec63f23fa86d5686c997d7f7
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-08 00:25:28 -0800

    update subsets 2

--- src/com/freetymekiyan/algorithms/level/Medium/Subsets2.java
+++ src/com/freetymekiyan/algorithms/level/Medium/Subsets2.java
@@ -35,62 +35,83 @@ public class Subsets2 {
             return Collections.emptyList();
         }
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(num); // Sort first
-        backtrack(res, new ArrayList<>(), num, 0);
+        Arrays.sort(num); // Sort first.
+        backtrack(res, num, 0, new ArrayList<>());
         return res;
     }
 
     /**
-     * Backtrack the array.
-     * Each call will generate a subset, dereference and add to the final result.
-     * Then for the rest of the numbers, backtrack each of them one by one.
-     * Remember to check duplicates by comparing current number and previous number.
-     * After the first position.
+     * Backtracking. DFS.
+     * Traverse a each node of a solution tree.
+     * For each number n in the nums array:
+     * | If n is a duplicate of previous number, skip.
+     * | Pick it and backtrack.
+     * | Remove it.
      */
-    private void backtrack(List<List<Integer>> res, List<Integer> list, int[] num, int pos) {
-        res.add(new ArrayList<>(list)); // Dereference.
-        for (int i = pos; i < num.length; i++) {
-            if (i != pos && num[i] == num[i - 1]) { // Check and skip duplicates
+    private void backtrack(List<List<Integer>> res, int[] nums, int pos, List<Integer> subsets) {
+        res.add(new ArrayList<>(subsets)); // Dereference.
+        for (int i = pos; i < nums.length; i++) {
+            if (i != pos && nums[i] == nums[i - 1]) { // Check and skip duplicates.
                 continue;
             }
-            list.add(num[i]); // Add.
-            backtrack(res, list, num, i + 1); // Backtrack down.
-            list.remove(list.size() - 1); // Reset.
+            subsets.add(nums[i]); // Add.
+            backtrack(res, nums, i + 1, subsets); // Backtrack.
+            subsets.remove(subsets.size() - 1); // Reset.
         }
     }
 
     /**
-     * Build subsets level by level from empty set.
-     * If a number from S is the first one of the numbers with the same value,
-     * it can be inserted to each previous subset and create new non-duplicate subsets.
-     * If a number from S is a duplicated number of some value,
-     * It only needs to extend subsets which contains its predecessor.
-     * <p>
-     * [1 2 2]
-     * [ ], [1], [2], [1 2]
-     * [1 2 2], [2 2] (add 2 to subsets which have 2)
+     * Backtracking.
+     * Duplicate with previous number will only be added if:
+     * The previous number is already in subset.
      */
-    public List<List<Integer>> subsetsWithDup2(int[] num) {
+    private void backtrackB(List<List<Integer>> res, int[] nums, int pos, List<Integer> subset) {
+        if (pos == nums.length) {
+            res.add(new ArrayList<>(subset)); // Dereference.
+            return;
+        }
+        subset.add(nums[pos]); // Add.
+        backtrack(res, nums, pos + 1, subset); // Backtrack.
+        subset.remove(subset.size() - 1); // Reset.
+        if (pos > 0 && nums[pos] == nums[pos - 1] && !subset.isEmpty() && nums[pos - 1] == subset
+            .get(subset.size() - 1)) {
+            return;
+        }
+        backtrack(res, nums, pos + 1, subset);
+    }
+
+    /**
+     * DP. Bottom-up.
+     * Build subsets level by level from empty set.
+     * For each number n in nums:
+     * | If n is not a duplicate:
+     * |   Insert n to each previous subsets and create new subsets.
+     * | If n is a duplicate:
+     * |   Only need to insert n to the subsets that contains a previous duplicate.
+     * E.g. [1 2 2]
+     * [] => [], [1] => [], [1], [2], [1 2] => [], [1], [2], [1 2], [2 2], [1 2 2]
+     * Add 2 to subsets which have 2, which is the latter half of result.
+     */
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>()); // Empty set
-        if (null == num || num.length == 0) {
+        res.add(new ArrayList<>()); // Empty set.
+        if (null == nums || nums.length == 0) {
             return res;
         }
-        Arrays.sort(num); // Sort first
+        Arrays.sort(nums); // Sort first.
 
         int j, prevSize = 0;
-        for (int i = 0; i < num.length; i++) {
-            if (i != 0 && num[i] == num[i - 1]) // Dup number
-            {
-                j = prevSize; // # of previous sets before last number
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) { // Duplicate.
+                j = prevSize; // # of previous sets before last number.
             } else {
-                j = 0;
+                j = 0; // No dup, start from beginning.
             }
-            prevSize = res.size(); // # of previous sets
-            /* Add to previous sets with same num */
+            prevSize = res.size(); // # of previous sets.
+            // Add to previous sets with same num
             for (; j < prevSize; j++) {
                 List<Integer> temp = new ArrayList<>(res.get(j));
-                temp.add(num[i]);
+                temp.add(nums[i]);
                 res.add(temp);
             }
         }
