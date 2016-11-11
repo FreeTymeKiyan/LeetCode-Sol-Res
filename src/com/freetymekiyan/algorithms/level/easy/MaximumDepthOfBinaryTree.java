4e33e654b3c1a4bcf1bb02ca3dfd26c2b1e1ced0
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-10 22:07:18 -0800

    update maximum depth of binary tree

--- src/com/freetymekiyan/algorithms/level/easy/MaximumDepthOfBinaryTree.java
+++ src/com/freetymekiyan/algorithms/level/easy/MaximumDepthOfBinaryTree.java
@@ -1,20 +1,29 @@
+package com.freetymekiyan.algorithms.level.easy;
+
+import com.freetymekiyan.algorithms.utils.Utils.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given a binary tree, find its maximum depth.
  * <p>
- * The maximum depth is the number of nodes along the longest path from the
- * root node down to the farthest leaf node.
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  * <p>
- * Tags: Tree, DFS
+ * Company Tags: LinkedIn, Uber, Apple, Yahoo
+ * Tags: Tree, Depth-first Search
+ * Similar Problems: (E) Balanced Binary Tree (E) Minimum Depth of Binary Tree
  */
-public class MaxDepth {
+public class MaximumDepthOfBinaryTree {
 
     /**
-     * Recursive, O(n)
-     * If tree is empty, return 0
-     * Else
-     * Get the max depth of left subtree recursively
-     * Get the max depth of right subtree recursively
-     * Get the max of max depths of left and right subtrees and add 1 to it
+     * Recursive. DFS. O(n) Time.
+     * Base case:
+     * If root is null, return 0.
+     * Recurrence relation:
+     * Depth of current node is the maximum of left depth and right depth + 1.
      */
     private int maxDepth(TreeNode root) {
         if (root == null) {
@@ -25,10 +34,65 @@ public class MaxDepth {
         return Math.max(left, right) + 1;
     }
 
-    class TreeNode {
+    /**
+     * Iterative. DFS. Stack.
+     * One stack for DFS. Another for depth.
+     * Push root onto stack to start.
+     * While stack is not empty:
+     * | Pop node from stack. Pop depth from the other stack.
+     * | Update max value.
+     * | Add left and right children to stack.
+     * Return max depth.
+     */
+    private int maxDepthB(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Deque<Integer> depths = new ArrayDeque<>();
+        stack.push(root);
+        depths.push(1);
+        int maxDepth = 0;
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int d = depths.pop();
+            maxDepth = Math.max(d, maxDepth);
+            if (node.left != null) {
+                stack.push(node.left);
+                depths.push(d + 1);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+                depths.push(d + 1);
+            }
+        }
+        return maxDepth;
+    }
 
-        int val;
-        TreeNode left;
-        TreeNode right;
+    /**
+     * BFS. Level Order Traversal.
+     * Increase depth for each level.
+     */
+    public int maxDepthC(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 }
