dd6269e6b3df0afb2e6b1778dbe966e94aed71cc
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-13 17:13:25 -0800

    update maximal square

--- src/com/freetymekiyan/algorithms/level/medium/MaximalSquare.java
+++ src/com/freetymekiyan/algorithms/level/medium/MaximalSquare.java
@@ -11,17 +11,22 @@ package com.freetymekiyan.algorithms.level.medium;
  * 1 0 0 1 0
  * Return 4.
  * <p>
+ * Company Tags: Apple, Airbnb, Facebook
  * Tags: Dynamic Programming
- * Similar: Problems (H) Maximal Rectangle
+ * Similar Problems: (H) Maximal Rectangle
  */
 public class MaximalSquare {
 
     /**
-     * DP, 2D matrix.
-     * The recurrence relation here is not easy to see.
-     * Use largest square's edge length formed including current grid.
-     * If the current grid is an 1,the edge length of current grid is the (minimum of its top, top-left, and left) + 1.
-     * Otherwise, if its 0, the edge length is still zero.
+     * DP.
+     * Finding the largest square's area is the same as finding the edge length.
+     * The recurrence relation here is:
+     * Suppose the largest edge length formed by current grid at matrix[i-1][j-1] is dp[i][j].
+     * If matrix[i - 1][j - 1] = 1, dp[i][j] = min(dp[i-1][j], dp[i][j], dp[i][j-1]) + 1.
+     * If matrix[i - 1][j - 1] = 0, dp[i][j] = 0.
+     * <p>
+     * To understand the recurrence relation, draw a matrix.
+     * Iff grid [i,j] is 1 and it's just a corners of other 1s, can the length expand.
      */
     public int maximalSquare(char[][] matrix) {
         if (matrix == null) {
@@ -29,22 +34,24 @@ public class MaximalSquare {
         }
         int r = matrix.length;
         int c = r == 0 ? 0 : matrix[0].length;
-        int[][] dp = new int[r + 1][c + 1];
+        int[][] dp = new int[r + 1][c + 1]; // First row and column are all 0.
         int maxLen = 0;
-        for (int i = 1; i <= r; i++) {
+        for (int i = 1; i <= r; i++) { // Traverse dp array. Note the equal sign.
             for (int j = 1; j <= c; j++) {
                 if (matrix[i - 1][j - 1] == '1') {
+                    // Update dp[i][j] and the max length.
                     dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                     maxLen = Math.max(maxLen, dp[i][j]);
                 }
             }
         }
-        return maxLen * maxLen; // Return AREA here
+        return maxLen * maxLen; // Return AREA here.
     }
 
     /**
-     * DP, 1D matrix.
-     * Reduce space usage to 1D matrix and an integer.
+     * DP. Space optimized.
+     * Only the previous row and previous column is needed.
+     * So reduce space usage to an array and an integer.
      */
     public int maximalSquareB(char[][] matrix) {
         if (matrix == null) {
@@ -52,22 +59,22 @@ public class MaximalSquare {
         }
         int r = matrix.length;
         int c = r == 0 ? 0 : matrix[0].length;
-        int[] dp = new int[c + 1]; // Only need one row
-        int prev = 0; // Store dp[i-1][j-1]
+        int[] dp = new int[c + 1]; // Only need one row.
+        int prev = 0; // Store dp[i-1][j-1].
         int maxLen = 0;
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= c; j++) {
-                int temp = dp[j]; // Store last dp[j] before update
+                int temp = dp[j]; // Store dp[i-1][j-1] of next iteration.
                 if (matrix[i - 1][j - 1] == '1') {
                     dp[j] = Math.min(Math.min(dp[j - 1], dp[j]), prev) + 1;
                     maxLen = Math.max(maxLen, dp[j]);
                 } else {
-                    dp[j] = 0;
+                    dp[j] = 0; // Have to update when grid is 0.
                 }
-                prev = temp; // dp[j] before udpate is the dp[i-1][j-1] for the next loop
+                prev = temp; // dp[j] before update is the dp[i-1][j-1] for the next loop.
             }
         }
-        return maxLen * maxLen; // Return the AREA here
+        return maxLen * maxLen; // Return the AREA here.
     }
 
 }
