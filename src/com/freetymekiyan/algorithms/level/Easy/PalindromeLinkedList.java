2c9cf31a285d1b192be1b1ac8170f2e1fd020181
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-14 21:33:00 -0800

    update palindrome linked list

--- src/com/freetymekiyan/algorithms/level/Easy/PalindromeLinkedList.java
+++ src/com/freetymekiyan/algorithms/level/Easy/PalindromeLinkedList.java
@@ -2,6 +2,7 @@ package com.freetymekiyan.algorithms.level.easy;
 
 import com.freetymekiyan.algorithms.utils.Utils;
 import com.freetymekiyan.algorithms.utils.Utils.ListNode;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,6 +14,7 @@ import org.junit.Test;
  * Follow up:
  * Could you do it in O(n) time and O(1) space?
  * <p>
+ * Company Tags: Amazon, Facebook
  * Tags: Linked List, Two Pointers
  * Similar Problems: (E) Palindrome Number, (E) Valid Palindrome, (E) Reverse Linked List
  */
@@ -21,10 +23,15 @@ public class PalindromeLinkedList {
     private PalindromeLinkedList p;
 
     /**
-     * Use two pointers, one slow pointer, one fast pointer.
-     * Move slow pointer to the middle.
-     * Reverse the right half of the list.
-     * Then compare.
+     * Two Pointers.
+     * Find the middle node, reverse the right half list, then check each node.
+     * Use two pointers, one slow pointer s, one fast pointer f.
+     * Move s to the head of the right half list.
+     * If there are odd number of nodes, move s one step further.
+     * Reverse the right half of the list starting from s.
+     * Then compare each node's value while s is not null.
+     * If diff, return false. Else continue.
+     * After all nodes checked, return true.
      */
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
@@ -36,15 +43,31 @@ public class PalindromeLinkedList {
             slow = slow.next;
             fast = fast.next.next;
         }
-        if (fast != null) slow = slow.next;
-        slow = reverseList(slow);
-        while (slow != null && head.val == slow.val) {
-            head = head.next;
+        if (fast != null) { // Make sure slow is always the head of right half.
             slow = slow.next;
         }
-        return slow == null;
+        slow = reverseList(slow); // Reverse the right half of the list.
+        ListNode cur = head;
+        while (slow != null) {
+            if (cur.val != slow.val) {
+                return false;
+            }
+            cur = cur.next;
+            slow = slow.next;
+        }
+        return true;
     }
 
+    /**
+     * Iterative.
+     * Create a new head as null, which will be the tail of the reversed list.
+     * While head is not null:
+     * | Store the next node.
+     * | Reverse head.
+     * | Update new head.
+     * | Move to next.
+     * Return new head.
+     */
     private ListNode reverseList(ListNode head) {
         ListNode newHead = null;
         while (head != null) {
