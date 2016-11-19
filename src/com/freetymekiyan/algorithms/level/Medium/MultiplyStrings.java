5b0d0086fd06a402da60cc5c69cd072a5fe182b2
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2016-11-19 10:08:56 -0800

    update multiply strings

--- src/com/freetymekiyan/algorithms/level/Medium/MultiplyStrings.java
+++ src/com/freetymekiyan/algorithms/level/Medium/MultiplyStrings.java
@@ -15,7 +15,7 @@ package com.freetymekiyan.algorithms.level.medium;
 public class MultiplyStrings {
 
     /**
-     * Math, String.
+     * Math. String.
      * How to do multiplication?
      * Start from right to left, multiply each pair of digits, and add them together.
      * Result num1[i] * num2[j] will be placed at i + j and i + j + 1.
@@ -28,24 +28,24 @@ public class MultiplyStrings {
         if (num1 == null || num2 == null) {
             return "";
         }
-        if (num1.equals("0") || num2.equals("0")) { // If one number is 0.
+        if ("0".equals(num1) || "0".equals(num2)) { // If one number is 0.
             return "0";
         }
         int m = num1.length(), n = num2.length();
-        int[] pos = new int[m + n]; // Length is at most m + n.
+        int[] product = new int[m + n]; // Length is at most m + n.
         // Pick one digit from one number, multiply with each digit in the other number.
-        for (int i = m - 1; i >= 0; i--) {
+        for (int i = m - 1; i >= 0; i--) { // From right to left, from lower significant digit to higher.
             for (int j = n - 1; j >= 0; j--) {
                 int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int p1 = i + j, p2 = i + j + 1; // p1 is a more significant digit than p2
-                int sum = mul + pos[p2];
-                pos[p1] += sum / 10; // Carry.
-                pos[p2] = sum % 10;
+                int p1 = i + j, p2 = i + j + 1; // p1 is a more significant digit than p2.
+                int sum = mul + product[p2];
+                product[p1] += sum / 10; // Carry.
+                product[p2] = sum % 10;
             }
         }
 
         StringBuilder ans = new StringBuilder();
-        for (int p : pos) {
+        for (int p : product) {
             if (p != 0 || ans.length() != 0) { // Not zero, or not empty.
                 ans.append(p);
             }
