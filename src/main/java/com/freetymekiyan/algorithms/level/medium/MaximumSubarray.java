e50f9ced3f7d6dfefbbbc02c4b3d33dc7a69b01f
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2017-12-20 19:57:16 -0800

    update 53. Maximum Subarray with divide and conquer and more explanation

--- src/main/java/com/freetymekiyan/algorithms/level/medium/MaximumSubarray.java
+++ src/main/java/com/freetymekiyan/algorithms/level/medium/MaximumSubarray.java
@@ -1,12 +1,12 @@
 package com.freetymekiyan.algorithms.level.medium;
 
 /**
+ * 53. Maximum Subarray
+ * <p>
  * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
  * <p>
  * For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
  * the contiguous subarray [4,-1,2,1] has the largest sum = 6.
- * <p>
- * click to show more practice.
  * <p>
  * More practice:
  * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which
@@ -20,12 +20,12 @@ public class MaximumSubarray {
 
 
     /**
-     * DP.
+     * DP. O(n) Time, O(1) Space.
+     * State: the max subarray sum ending at i.
      * Recurrence Relation:
-     * So if we know the max sum ending at i-1. How can we get the max sum ending at i?
-     * Since the sub array is contiguous. It must include nums[i].
-     * So max sum ending at i is either nums[i] or nums[i] + max sum ending at i-1, whichever is larger.
-     * Then we update the overall result with the new max.
+     * Since the sub array is contiguous, nums[i] must be included.
+     * So the max subarray sum ending at i is either nums[i] or nums[i] + max sum ending at i-1, whichever is larger.
+     * The overall maximum is the max of all maxEndingHere's.
      */
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
@@ -34,26 +34,6 @@ public class MaximumSubarray {
         for (int i = 1; i < nums.length; ++i) {
             maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
             max = Math.max(max, maxEndingHere);
-        }
-        return max;
-    }
-
-    /**
-     * DP, O(n) Time, O(1) Space
-     * If A[i] < 0 && currentMax + A[i] < 0, should recalculate max
-     * If A[i] < 0 && currentMax + A[i] >= 0, continue
-     * currentMax = max(currentMax + A[i], A[i])
-     * maxSubArr = max(currentMax, maxSubArr)
-     */
-    public int maxSubArraySum(int[] A) {
-        if (A == null || A.length == 0) {
-            return 0;
-        }
-        int curMax = A[0];
-        int max = A[0];
-        for (int i = 1; i < A.length; i++) { // note that i starts from 1
-            curMax = Math.max(curMax + A[i], A[i]);
-            max = Math.max(curMax, max);
         }
         return max;
     }
@@ -103,6 +83,53 @@ public class MaximumSubarray {
             }
         }
         return new int[]{begin, end, maxSoFar};
+    }
+
+    /**
+     * Divide and conquer.
+     * Recurrence:
+     * The max sub array sum of array nums can be divided into 3 parts:
+     * 1. Only in left half.
+     * 2. Only in right half.
+     * 3. In both halves.
+     * The result should be the largest of these 3.
+     * Base case:
+     * When there is only 1 element in the array, just return the element.
+     */
+    public int maxSubArray3(int[] nums) {
+        return maxSubArray(nums, 0, nums.length - 1);
+    }
+
+    private int maxSubArray(int[] nums, int start, int end) {
+        if (start == end) return nums[start];
+        int mid = start + (end - start) / 2;
+        int max1 = maxSubArray(nums, start, mid);
+        int max2 = maxSubArray(nums, mid + 1, end);
+        int max3 = crossMidMaxSum(nums, start, end, mid);
+        return Math.max(max1, Math.max(max2, max3));
+    }
+
+    /**
+     * The max subarray sum cross the mid point.
+     * Must have nums[mid] since the array is continuous.
+     * Find the maximum of left side, then right side.
+     * The result is their sum.
+     */
+    private int crossMidMaxSum(int[] nums, int start, int end, int mid) {
+        int leftMax = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = mid; i >= start; i--) {
+            sum += nums[i];
+            if (sum > leftMax) leftMax = sum;
+        }
+
+        int rightMax = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = mid + 1; i <= end; i++) {
+            sum += nums[i];
+            if (sum > rightMax) rightMax = sum;
+        }
+        return leftMax + rightMax;
     }
 
 }
