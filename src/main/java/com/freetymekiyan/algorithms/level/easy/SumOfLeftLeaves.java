c32547275a0fa08654727cb01f4b328a33524cf6
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2017-12-21 20:51:00 -0800

    update 404. Sum of Left Leaves with more traversals

--- src/main/java/com/freetymekiyan/algorithms/level/easy/SumOfLeftLeaves.java
+++ src/main/java/com/freetymekiyan/algorithms/level/easy/SumOfLeftLeaves.java
@@ -2,7 +2,13 @@ package com.freetymekiyan.algorithms.level.easy;
 
 import com.freetymekiyan.algorithms.utils.Utils.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
+ * 404. Sum of Left Leaves
+ * <p>
  * Find the sum of all left leaves in a given binary tree.
  * <p>
  * Example:
@@ -15,13 +21,12 @@ import com.freetymekiyan.algorithms.utils.Utils.TreeNode;
  * <p>
  * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
  * Company Tags: Facebook
- * Tags: Tree
+ * Related Topics: Tree
  */
 public class SumOfLeftLeaves {
 
     /**
      * DFS. Recursive.
-     * Just change the visit of DFS.
      * Recurrence relation:
      * Current left leaf's value + sum of left subtree's leaves + sum to right subtree's leaves.
      * When visit, if current node's left child is not null, and it is a leaf.
@@ -35,7 +40,7 @@ public class SumOfLeftLeaves {
             return 0;
         }
         int sum = 0;
-        if (root.left != null && root.left.left == null && root.left.right == null) { // root.left is a leave.
+        if (root.left != null && root.left.left == null && root.left.right == null) { // root.left is a leaf.
             sum += root.left.val; // Add directly to result.
         } else { // root.left is a subtree, recurse.
             sum += sumOfLeftLeaves(root.left);
@@ -48,16 +53,16 @@ public class SumOfLeftLeaves {
      * DFS. Recursive.
      * Pass another boolean to indication whether the call is from left.
      */
-    public int sumOfLeftLeavesB(TreeNode root) {
+    public int sumOfLeftLeaves2(TreeNode root) {
         return dfs(root, false);
     }
 
     /**
      * DFS.
-     * Two base cases:
+     * Base case:
      * 1) root is null, return 0.
-     * 2) root not null, root is leaf. If from left, return value. Otherwise return 0.
-     * The result is the left leaves sum of left subtree and right subtree.
+     * 2) If current node is a leaf, and it's from left, return it's value.
+     * The result is the sum of left leaves sum of left subtree and right subtree.
      */
     private int dfs(TreeNode root, boolean isLeft) {
         if (root == null) {
@@ -69,4 +74,51 @@ public class SumOfLeftLeaves {
         return dfs(root.left, true) + dfs(root.right, false);
     }
 
+    /**
+     * Pre-order traversal with a stack.
+     * For each node, check if it has a left leaf.
+     * If so, add the value to sum.
+     */
+    public int sumOfLeftLeaves3(TreeNode root) {
+        if (root == null) return 0;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        int sum = 0; // Avoid overflow with long? sum should be within integer range.
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                if (node.left.left == null && node.left.right == null) {
+                    sum += node.left.val;
+                }
+                stack.push(node.left);
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * Level-order traversal or BFS.
+     */
+    public int sumOfLeftLeaves4(TreeNode root) {
+        if (root == null) return 0;
+        int sum = 0;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                queue.offer(node.left);
+                if (node.left.left == null && node.left.right == null) {
+                    sum += node.left.val;
+                }
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return sum;
+    }
 }
