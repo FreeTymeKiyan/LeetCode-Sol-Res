453837ccc16315afa145ad8a5cf6f8dacf61931b
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2017-12-24 18:03:42 -0800

    update 314. Binary Tree Vertical Order Traversal with some thoughts and explanation and better naming

--- src/main/java/com/freetymekiyan/algorithms/level/medium/BinaryTreeVerticalOrderTraversal.java
+++ src/main/java/com/freetymekiyan/algorithms/level/medium/BinaryTreeVerticalOrderTraversal.java
@@ -2,15 +2,11 @@ package com.freetymekiyan.algorithms.level.medium;
 
 import com.freetymekiyan.algorithms.utils.Utils.TreeNode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 /**
+ * 314. Binary Tree Vertical Order Traversal
+ * <p>
  * Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by
  * column).
  * <p>
@@ -76,51 +72,60 @@ public class BinaryTreeVerticalOrderTraversal {
 
     /**
      * BFS, Hash Table.
+     * <p>
+     * Vertical traversal is viewing a tree from left to right, from top to bottom.
+     * We can divide the tree nodes into rows and columns, like a table.
+     * When we traverse the tree:
+     * | 1. if it's a left child, it should have a decremented column value.
+     * | 2. if it's a right child, it should have a incremented column value.
+     * <p>
      * Use a map to store level and the node values in it.
+     * | Since we don't know the level range yet, we can only use map.
      * Use two integers, min and max, to track the range of levels.
-     * And retrieve result from the map.
-     * Use a queue for BFS on tree nodes.
-     * Use another separate queue to store levels.
+     * | Then we can use this range we retrieve levels from the map.
+     * Use a queue to traverse tree nodes in a level order.
+     * | If left child is added to queue before right child, it will be in the front of the level.
+     * Use another separate queue to store levels as integers.
+     * | Otherwise we would have to modify the data structure of tree node.
      * Initialize level value of root as 0.
-     * When accessing a left child, level value decrement by 1.
-     * When accessing right, level value increment by 1.
+     * When accessing a left child, level value decrements by 1.
+     * When accessing right, level value increments by 1.
      */
     public List<List<Integer>> verticalOrder(TreeNode root) {
         if (root == null) {
             return Collections.emptyList();
         }
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, List<Integer>> rowToNode = new HashMap<>();
         int min = 0; // Level range.
         int max = 0;
-        Queue<TreeNode> queue = new LinkedList<>();
-        Queue<Integer> level = new LinkedList<>();
-        queue.add(root);
-        level.add(0);
-        // BFS, put each node in its level.
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            int curLvl = level.poll();
-            if (!map.containsKey(curLvl)) {
-                map.put(curLvl, new ArrayList<>());
+        Queue<TreeNode> nodes = new ArrayDeque<>();
+        Queue<Integer> levels = new ArrayDeque<>();
+        nodes.offer(root);
+        levels.offer(0);
+        // BFS or level order traversal.
+        while (!nodes.isEmpty()) {
+            TreeNode node = nodes.poll();
+            int level = levels.poll();
+            if (!rowToNode.containsKey(level)) { // Make sure each level has a list ready.
+                rowToNode.put(level, new ArrayList<>());
             }
-            map.get(curLvl).add(node.val);
+            rowToNode.get(level).add(node.val);
             if (node.left != null) {
-                queue.add(node.left);
-                level.add(curLvl - 1);
-                min = Math.min(min, curLvl - 1);
+                nodes.offer(node.left);
+                levels.offer(level - 1);
+                min = Math.min(min, level - 1);
             }
             if (node.right != null) {
-                queue.add(node.right);
-                level.add(curLvl + 1);
-                max = Math.max(max, curLvl + 1);
+                nodes.add(node.right);
+                levels.add(level + 1);
+                max = Math.max(max, level + 1);
             }
         }
-        // Retrieve result from map.
+        // Retrieve result from the map.
         List<List<Integer>> res = new ArrayList<>();
         for (int i = min; i <= max; i++) {
-            res.add(map.get(i));
+            res.add(rowToNode.get(i));
         }
         return res;
     }
-
 }
