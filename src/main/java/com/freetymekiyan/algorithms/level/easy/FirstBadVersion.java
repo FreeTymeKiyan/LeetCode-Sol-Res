081737b78f44d048d23ccc0a79b96409c2270554
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2017-12-25 21:42:08 -0800

    update 278. First Bad Version

--- src/main/java/com/freetymekiyan/algorithms/level/easy/FirstBadVersion.java
+++ src/main/java/com/freetymekiyan/algorithms/level/easy/FirstBadVersion.java
@@ -1,6 +1,8 @@
 package com.freetymekiyan.algorithms.level.easy;
 
 /**
+ * 278. First Bad Version
+ * <p>
  * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of
  * your product fails the quality check. Since each version is developed based on the previous version, all the versions
  * after a bad version are also bad.
