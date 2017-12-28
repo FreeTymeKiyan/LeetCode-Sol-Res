d691992299b04ea123283a4464e49d4e6263e76c
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2017-12-27 22:07:06 -0800

    update 265. Paint House 2

--- src/main/java/com/freetymekiyan/algorithms/level/hard/PaintHouse2.java
+++ src/main/java/com/freetymekiyan/algorithms/level/hard/PaintHouse2.java
@@ -1,6 +1,8 @@
 package com.freetymekiyan.algorithms.level.hard;
 
 /**
+ * 265. Paint House 2
+ * <p>
  * There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with
  * a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
  * <p>
