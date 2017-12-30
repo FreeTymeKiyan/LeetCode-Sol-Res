17cc09e2f03bffe37ab2c05327f3a82dc0b867ef
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2017-12-29 20:59:01 -0800

    add tests to 215. Kth Largest Element in an Array

---
+++ src/test/java/com/freetymekiyan/algorithms/level/medium/KthLargestElementInAnArrayTest.java
@@ -0,0 +1,33 @@
+package com.freetymekiyan.algorithms.level.medium;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class KthLargestElementInAnArrayTest {
+
+    @DataProvider(name = "examples")
+    public Object[][] getExamples() {
+        return new Object[][]{
+                new Object[]{new int[]{-1, 2, 0}, 2, 0},
+                new Object[]{new int[]{-1, 2, 0}, 1, 2},
+                new Object[]{new int[]{2, 1}, 1, 2},
+                new Object[]{new int[]{2, 1}, 2, 1},
+                new Object[]{new int[]{1}, 1, 1}
+        };
+    }
+
+    @Test
+    public void testFindKthLargest() {
+    }
+
+    @Test
+    public void testFindKthLargest2() {
+    }
+
+    @Test(dataProvider = "examples")
+    public void testFindKthLargest3(int[] nums, int k, int output) {
+        KthLargestElementInAnArray kth = new KthLargestElementInAnArray();
+        Assert.assertEquals(kth.findKthLargest3(nums, k), output);
+    }
+}
