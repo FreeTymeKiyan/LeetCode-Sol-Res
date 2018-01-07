5ae0beafa305d4df6dc603e16ed8a258c5e42e7c
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2018-01-06 23:14:12 -0800

    update 85. Maximal Rectangle

--- src/main/java/com/freetymekiyan/algorithms/level/hard/MaximalRectangle.java
+++ src/main/java/com/freetymekiyan/algorithms/level/hard/MaximalRectangle.java
@@ -3,6 +3,8 @@ package com.freetymekiyan.algorithms.level.hard;
 import java.util.Arrays;
 
 /**
+ * 85. Maximal Rectangle
+ * <p>
  * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
  * <p>
  * For example, given the following matrix:
@@ -22,25 +24,36 @@ public class MaximalRectangle {
     /**
      * DP.
      * Three matrices: left, right, and height.
-     * Maximal rectangle area at row i and column j is: [right(i,j) - left(i,j)] * height(i,j).
-     * left is the left boundary of current row and previous row.
-     * left(i,j) = max(left(i-1,j), curLeft)
-     * curLeft is the left boundary candidate of current row.
+     * Height is number of continuous 1's that end at j.
+     * Left is the left boundary of this height. The actual value is an index.
+     * Right is the right boundary of this right. The actual value is index + 1.
+     * The rectangle area of this height at row i and column j is: [right(i,j) - left(i,j)] * height(i,j).
      * <p>
-     * right is the right boundary of current row. Actually is index + 1 for easy implementation.
-     * right(i,j) = min(right(i-1,j), curRight)
-     * curRight is the right boundary candidate of current row.
+     * Recurrence relations:
+     * left(i,j) = max(left(i-1,j), leftBound)
+     * leftBound is the leftmost 1 of this height at current row.
+     * All left boundaries are initialized as 0, which is the leftmost possible.
      * <p>
-     * height is how many 1s, including current 1, from top to current position.
+     * right(i,j) = min(right(i-1,j), rightBound)
+     * rightBound is the rightmost 1 of this height at current row + 1.
+     * All right boundaries are initialized as n, which is the rightmost possible.
+     * <p>
      * height(i,j) = height(i-1,j) + 1, if matrix[i][j]=='1'.
      * height(i,j) = 0, if matrix[i][j]=='0'.
      * <p>
      * Implementation:
      * Initialize left array and height as all zeroes, right array as the column length.
      * For each row in the matrix, update height, left, right arrays.
-     * Then compute the max area.
-     * Stop when all rows are looped through.
+     * Then compute the area and record the maximum.
+     * Stop when all grids are done.
      * https://discuss.leetcode.com/topic/6650/share-my-dp-solution
+     * <p>
+     * Note that the rectangle area we get is not the maximum at each grid, rather, it's the area of the rectangle of
+     * the maximum possible height.
+     * Why does that covers the maximum rectangle?
+     * Because:
+     * If the max rectangle has only 1 column, the bottom grid will have the max area.
+     * If the max rectangle has > 1 columns, the bottom row will have the max area.
      */
     public int maximalRectangle(char[][] matrix) {
         int m = matrix.length;
@@ -51,7 +64,7 @@ public class MaximalRectangle {
 //        Arrays.fill(left, 0);
         Arrays.fill(right, n);
 //        Arrays.fill(height, 0);
-        int maxA = 0;
+        int max = 0;
         for (int i = 0; i < m; i++) {
             // Compute height (can do this from either side).
             for (int j = 0; j < n; j++) {
@@ -61,31 +74,31 @@ public class MaximalRectangle {
                     height[j] = 0;
                 }
             }
-            // Compute left (must from left to right).
-            int curLeft = 0; // Index of leftmost 1 of current row.
+            // Compute left boundaries (must from left to right).
+            int leftBound = 0; // Index of leftmost 1 of current row.
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == '1') {
-                    left[j] = Math.max(left[j], curLeft);
+                    left[j] = Math.max(left[j], leftBound);
                 } else {
                     left[j] = 0;
-                    curLeft = j + 1;
+                    leftBound = j + 1;
                 }
             }
-            // Compute right (must from right to left).
-            int curRight = n; // Index+1 of rightmost 1 of current row.
+            // Compute right boundaries (must from right to left).
+            int rightBound = n; // Index + 1 of rightmost 1 of current row.
             for (int j = n - 1; j >= 0; j--) {
                 if (matrix[i][j] == '1') {
-                    right[j] = Math.min(right[j], curRight);
+                    right[j] = Math.min(right[j], rightBound);
                 } else {
                     right[j] = n; // Like reset. Make sure right[j] >= curRight.
-                    curRight = j;
+                    rightBound = j;
                 }
             }
             // Compute the area of rectangle (can do this from either side).
             for (int j = 0; j < n; j++) {
-                maxA = Math.max(maxA, (right[j] - left[j]) * height[j]);
+                max = Math.max(max, (right[j] - left[j]) * height[j]);
             }
         }
-        return maxA;
+        return max;
     }
 }
