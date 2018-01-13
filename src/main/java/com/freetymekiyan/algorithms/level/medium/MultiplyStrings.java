33e4162e08c76361599660b009afc53d23bafa61
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2018-01-12 22:26:43 -0800

    update 43. Multiply Strings

--- src/main/java/com/freetymekiyan/algorithms/level/medium/MultiplyStrings.java
+++ src/main/java/com/freetymekiyan/algorithms/level/medium/MultiplyStrings.java
@@ -1,6 +1,8 @@
 package com.freetymekiyan.algorithms.level.medium;
 
 /**
+ * 43. Multiply Strings
+ * <p>
  * Given two numbers represented as strings, return multiplication of the numbers as a string.
  * <p>
  * Note:
@@ -35,21 +37,21 @@ public class MultiplyStrings {
         int[] product = new int[m + n]; // Length is at most m + n.
         // Pick one digit from one number, multiply with each digit in the other number.
         for (int i = m - 1; i >= 0; i--) { // From right to left, from lower significant digit to higher.
+            int x = num1.charAt(i) - '0';
             for (int j = n - 1; j >= 0; j--) {
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int p1 = i + j, p2 = i + j + 1; // p1 is a more significant digit than p2.
-                int sum = mul + product[p2];
-                product[p1] += sum / 10; // Carry.
-                product[p2] = sum % 10;
+                int mul = x * (num2.charAt(j) - '0');
+                product[i + j + 1] += mul;
+                product[i + j] += product[i + j + 1] / 10; // Carry.
+                product[i + j + 1] %= 10; // i + j + 1 will cover every digit and make them < 10.
             }
         }
 
-        StringBuilder ans = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         for (int p : product) {
-            if (p != 0 || ans.length() != 0) { // Not zero, or not empty.
-                ans.append(p);
+            if (p != 0 || res.length() != 0) { // Non-zero should definitely be in result. If something is already in result, everything after should be appended.
+                res.append(p);
             }
         }
-        return ans.toString();
+        return res.toString();
     }
 }
