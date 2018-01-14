11317af5126fa6ebcfa1ed0c22f3b01b1510501d
FreeTymeKiyan <yangliu@ChangdeMBP.T-mobile.com>
2018-01-13 19:49:06 -0800

    update 26. Remove Duplicates from Sorted Array

--- src/main/java/com/freetymekiyan/algorithms/level/easy/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/freetymekiyan/algorithms/level/easy/RemoveDuplicatesFromSortedArray.java
@@ -1,6 +1,8 @@
 package com.freetymekiyan.algorithms.level.easy;
 
 /**
+ * 26. Remove Duplicates from Sorted Array
+ * <p>
  * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new
  * length.
  * <p>
@@ -20,17 +22,31 @@ public class RemoveDuplicatesFromSortedArray {
 
     /**
      * Two Pointers.
-     * Use the length to be returned as a pointer to the next position to be filled.
+     * Use the length of the distinct elements as a pointer to the next position to be filled.
      * For each number n in nums:
-     * | If length is 0 or n > nums[length - 1]:
+     * | If length is 0 or n != nums[length - 1]:
      * |   Update nums[length] to n. Increment length by 1.
      * Return length.
      */
-    public static int removeDupB(int[] nums) {
+    public int removeDuplicates(int[] nums) {
         int len = 0;
         for (int n : nums) {
-            if (len < 1 || n > nums[len - 1]) {
+            if (len == 0 || n != nums[len - 1]) {
                 nums[len++] = n;
+            }
+        }
+        return len;
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        int len = 1;
+        for (int i = 1, j = i; i < nums.length; i = j) {
+            while (j < nums.length && nums[j] == nums[len - 1]) {
+                j++;
+            }
+            if (j < nums.length) {
+                nums[len] = nums[j];
+                len++;
             }
         }
         return len;
