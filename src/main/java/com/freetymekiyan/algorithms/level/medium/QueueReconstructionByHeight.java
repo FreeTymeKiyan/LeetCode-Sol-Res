16a432edd4e15946579725269a4d00a34c8655bf
FreeTymeKiyan <notyouremail@unknown.com>
2019-09-07 18:28:53 -0700

    update 406 Queue Reconstruction by Height

--- src/main/java/com/freetymekiyan/algorithms/level/medium/QueueReconstructionByHeight.java
+++ src/main/java/com/freetymekiyan/algorithms/level/medium/QueueReconstructionByHeight.java
@@ -26,28 +26,39 @@ import java.util.List;
  */
 public class QueueReconstructionByHeight {
 
-    /**
-     * Sort the array by height descending and k ascending.
-     * Pick the highest people from the array.
-     * Then pick the second highest from the array and insert to result according to k.
-     * Repeat till there is no more people.
-     * <p>
-     * If k means the # of people in front of him that are shorter or equal.
-     * We should sort by height ascending and k asceding instead.
-     */
-    public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o2[0] - o1[0] == 0 ? o1[1] - o2[1] : o2[0] - o1[0];
-            }
-        });
-        List<int[]> res = new ArrayList<>();
-        for (int[] p : people) {
-            res.add(p[1], p);
-        }
-
-        return res.toArray(new int[0][0]);
+  /**
+   * Sort the array by height descending and k ascending.
+   * Pick the highest people from the array.
+   * Then pick the second highest from the array and insert to result according to k.
+   * Repeat till there is no more people.
+   * <p>
+   * If k means the # of people in front of him that are shorter or equal.
+   * We should sort by height ascending and k ascending instead.
+   */
+  public int[][] reconstructQueue(int[][] people) {
+    Arrays.sort(people, (p1, p2) -> p2[0] - p1[0] == 0 ? p1[1] - p2[1] : p2[0] - p1[0]);
+    final List<int[]> res = new ArrayList<>(people.length);
+    for (int[] p : people) {
+      res.add(p[1], p);
     }
+    return res.toArray(new int[0][0]);
+  }
 
+  /**
+   * Same idea.
+   * But comparator is faster than lambda in execution.
+   */
+  public int[][] reconstructQueue2(int[][] people) {
+    Arrays.sort(people, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] person1, int[] person2) {
+        return person2[0] - person1[0] == 0 ? person1[1] - person2[1] : person2[0] - person1[0];
+      }
+    });
+    final List<int[]> res = new ArrayList<>(people.length);
+    for (int[] p : people) {
+      res.add(p[1], p);
+    }
+    return res.toArray(new int[0][0]);
+  }
 }
