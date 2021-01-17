21a7b5bf4a42a205061ad0d6aef54b9d343f87aa
FreeTymeKiyan <notyouremail@unknown.com>
2021-01-17 15:21:36 -0800

    Update 412 FizzBuzz with one more solution and unit tests

--- src/main/java/com/freetymekiyan/algorithms/level/easy/FizzBuzz.java
+++ src/main/java/com/freetymekiyan/algorithms/level/easy/FizzBuzz.java
@@ -41,6 +41,11 @@ public class FizzBuzz {
   private static final String FIZZ = "Fizz";
   private static final String BUZZ = "Buzz";
 
+  /**
+   * A simpler version.
+   * If a number is both divisible by 3 and 5, both "Fizz" and "Buzz" will be added to the string builder.
+   * There's no need for another special case to add "FizzBuzz".
+   */
   public List<String> fizzBuzz(int n) {
     final List<String> res = new ArrayList<>();
     for (int i = 1; i <= n; i++) {
@@ -57,5 +62,30 @@ public class FizzBuzz {
       res.add(builder.toString());
     }
     return res;
+  }
+
+  /**
+   * A more verbose version by adding each case strictly.
+   */
+  public List<String> fizzBuzz2(int n) {
+    final List<String> ans = new ArrayList<>();
+    for (int num = 1; num <= n; num++) {
+      boolean divisibleBy3 = (num % 3 == 0);
+      boolean divisibleBy5 = (num % 5 == 0);
+      if (divisibleBy3 && divisibleBy5) {
+        // Divides by both 3 and 5, add FizzBuzz
+        ans.add("FizzBuzz");
+      } else if (divisibleBy3) {
+        // Divides by 3, add Fizz
+        ans.add("Fizz");
+      } else if (divisibleBy5) {
+        // Divides by 5, add Buzz
+        ans.add("Buzz");
+      } else {
+        // Not divisible by 3 or 5, add the number
+        ans.add(Integer.toString(num));
+      }
+    }
+    return ans;
   }
 }
--- src/test/java/com/freetymekiyan/algorithms/level/easy/FizzBuzzTest.java
+++ src/test/java/com/freetymekiyan/algorithms/level/easy/FizzBuzzTest.java
@@ -7,27 +7,32 @@ import java.util.List;
 
 public class FizzBuzzTest {
 
-    @Test
-    public void testFizzBuzz() {
-        int n = 15;
-        List<String> expected = List.of(
-                "1",
-                "2",
-                "Fizz",
-                "4",
-                "Buzz",
-                "Fizz",
-                "7",
-                "8",
-                "Fizz",
-                "Buzz",
-                "11",
-                "Fizz",
-                "13",
-                "14",
-                "FizzBuzz");
-        FizzBuzz f = new FizzBuzz();
-        Assert.assertEquals(f.fizzBuzz(15), expected);
+  private static final List<String> EXPECTED = List.of(
+      "1",
+      "2",
+      "Fizz",
+      "4",
+      "Buzz",
+      "Fizz",
+      "7",
+      "8",
+      "Fizz",
+      "Buzz",
+      "11",
+      "Fizz",
+      "13",
+      "14",
+      "FizzBuzz");
 
-    }
+  @Test
+  public void testFizzBuzz() {
+    FizzBuzz f = new FizzBuzz();
+    Assert.assertEquals(f.fizzBuzz(15), EXPECTED);
+  }
+
+  @Test
+  public void testFizzBuzz2() {
+    FizzBuzz f = new FizzBuzz();
+    Assert.assertEquals(f.fizzBuzz2(15), EXPECTED);
+  }
 }
